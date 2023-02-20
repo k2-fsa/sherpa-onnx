bf0be7d4677fe6e67fe1cc90d17880a51606c738
Fangjun Kuang <csukuangfj@gmail.com>
2023-02-20 16:46:44 +0800

    Begin to add zipformer

---
+++ sherpa-onnx/csrc/online-zipformer-transducer-model.cc
@@ -0,0 +1,12 @@
+// sherpa-onnx/csrc/online-zipformer-transducer-model.cc
+//
+// Copyright (c)  2023  Xiaomi Corporation
+
+#include "sherpa-onnx/csrc/online-zipformer-transducer-model.h"
+
+namespace sherpa_onnx {
+
+OnlineZipformerTransducerModel::OnlineZipformerTransducerModel(
+    const OnlineTransducerModelConfig &config) {}
+
+}  // namespace sherpa_onnx
