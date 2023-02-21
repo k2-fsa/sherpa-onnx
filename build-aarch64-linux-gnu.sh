d1994f1fd89b34b19d617920be706f42e1f9634e
Fangjun Kuang <csukuangfj@gmail.com>
2023-02-21 22:04:21 +0800

    Support cross compiling for aarch64 (#52)

---
+++ build-aarch64-linux-gnu.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+if ! command -v aarch64-linux-gnu-gcc  &> /dev/null; then
+  echo "Please install a toolchain for cross-compiling."
+  echo "You can refer to: "
+  echo "  https://k2-fsa.github.io/sherpa/onnx/install/aarch64-embedded-linux.html"
+  echo "for help."
+  exit 1
+fi
+
+set -ex
+
+dir=build-aarch64-linux-gnu
+mkdir -p $dir
+cd $dir
+
+if [ ! -f alsa-lib/src/.libs/libasound.so ]; then
+  echo "Start to cross-compile alsa-lib"
+  if [ ! -d alsa-lib ]; then
+    git clone --depth 1 https://github.com/alsa-project/alsa-lib
+  fi
+  # If it shows:
+  #  ./gitcompile: line 79: libtoolize: command not found
+  # Please use:
+  #  sudo apt-get install libtool m4 automake
+  #
+  pushd alsa-lib
+  CC=aarch64-linux-gnu-gcc ./gitcompile --host=aarch64-linux-gnu
+  popd
+  echo "Finish cross-compiling alsa-lib"
+fi
+
+export CPLUS_INCLUDE_PATH=$PWD/alsa-lib/include:$CPLUS_INCLUDE_PATH
+export SHERPA_ONNX_ALSA_LIB_DIR=$PWD/alsa-lib/src/.libs
+
+cmake \
+  -DCMAKE_INSTALL_PREFIX=./install \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DSHERPA_ONNX_ENABLE_TESTS=OFF \
+  -DSHERPA_ONNX_ENABLE_PYTHON=OFF \
+  -DCMAKE_TOOLCHAIN_FILE=../toolchains/aarch64-linux-gnu.toolchain.cmake \
+  ..
+
+make VERBOSE=1 -j4
+make install/strip
+
+# Enable it if only needed
+# cp -v $SHERPA_ONNX_ALSA_LIB_DIR/libasound.so* ./install/lib/
---
+++ sherpa-onnx/csrc/display.h
@@ -0,0 +1,79 @@
+// sherpa-onnx/csrc/display.h
+//
+// Copyright (c)  2022-2023  Xiaomi Corporation
+
+#ifndef SHERPA_ONNX_CSRC_DISPLAY_H_
+#define SHERPA_ONNX_CSRC_DISPLAY_H_
+#include <stdio.h>
+
+#include <string>
+
+namespace sherpa_onnx {
+
+class Display {
+ public:
+  void Print(int32_t segment_id, const std::string &s) {
+#ifdef _MSC_VER
+    fprintf(stderr, "%d:%s\n", segment_id, s.c_str());
+    return;
+#endif
+    if (last_segment_ == segment_id) {
+      Clear();
+    } else {
+      if (last_segment_ != -1) {
+        fprintf(stderr, "\n\r");
+      }
+      last_segment_ = segment_id;
+      num_previous_lines_ = 0;
+    }
+
+    fprintf(stderr, "\r%d:", segment_id);
+
+    int32_t i = 0;
+    for (size_t n = 0; n < s.size();) {
+      if (s[n] > 0 && s[n] < 0x7f) {
+        fprintf(stderr, "%c", s[n]);
+        ++n;
+      } else {
+        // Each Chinese character occupies 3 bytes for UTF-8 encoding.
+        std::string tmp(s.begin() + n, s.begin() + n + 3);
+        fprintf(stderr, "%s", tmp.data());
+        n += 3;
+      }
+
+      ++i;
+      if (i >= max_word_per_line_ && n + 1 < s.size() &&
+          (s[n] == ' ' || s[n] < 0)) {
+        fprintf(stderr, "\n\r ");
+        ++num_previous_lines_;
+        i = 0;
+      }
+    }
+  }
+
+ private:
+  // Clear the output for the current segment
+  void Clear() {
+    ClearCurrentLine();
+    while (num_previous_lines_ > 0) {
+      GoUpOneLine();
+      ClearCurrentLine();
+      --num_previous_lines_;
+    }
+  }
+
+  // Clear the current line
+  void ClearCurrentLine() const { fprintf(stderr, "\33[2K\r"); }
+
+  // Move the cursor to the previous line
+  void GoUpOneLine() const { fprintf(stderr, "\033[1A\r"); }
+
+ private:
+  int32_t max_word_per_line_ = 60;
+  int32_t num_previous_lines_ = 0;
+  int32_t last_segment_ = -1;
+};
+
+}  // namespace sherpa_onnx
+
+#endif  // SHERPA_ONNX_CSRC_DISPLAY_H_
