1b105dd2fcf6a82456449ff7d4a572ef1797fe52
Fangjun Kuang <csukuangfj@gmail.com>
2023-02-25 15:22:52 +0800

    Begin to add ios

---
+++ ios-swift/SherpaOnnx/SherpaOnnx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SherpaOnnx
+//
+//  Created by fangjun on 2023/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-swift/SherpaOnnx/SherpaOnnxUITests/SherpaOnnxUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SherpaOnnxUITestsLaunchTests.swift
+//  SherpaOnnxUITests
+//
+//  Created by fangjun on 2023/2/25.
+//
+
+import XCTest
+
+final class SherpaOnnxUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
