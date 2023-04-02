c61d682025870cd25a76e68709e18b542e5a6238
zx <12345678>
2023-04-02 09:11:38 +0800

    adding a python api for offline decode,and you can use offline-decode-files.py decoding wav-files by onnx model.

--- sherpa-onnx/python/csrc/offline-transducer-model-config.cc
+++ sherpa-onnx/python/csrc/offline-transducer-model-config.cc
@@ -15,8 +15,10 @@ namespace sherpa_onnx {
 void PybindOfflineTransducerModelConfig(py::module *m) {
   using PyClass = OfflineTransducerModelConfig;
   py::class_<PyClass>(*m, "OfflineTransducerModelConfig")
-      .def(py::init<const std::string &,const std::string &,const std::string &>(),
-           py::arg("encoder_filename"),py::arg("decoder_filename"), py::arg("joiner_filename"))
+      .def(py::init<const std::string &,const std::string &,
+               const std::string &>(),
+           py::arg("encoder_filename"),py::arg("decoder_filename"),
+           py::arg("joiner_filename"))
       .def_readwrite("encoder_filename", &PyClass::encoder_filename)
       .def_readwrite("decoder_filename", &PyClass::decoder_filename)
       .def_readwrite("joiner_filename", &PyClass::joiner_filename)
