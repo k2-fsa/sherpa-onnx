a54fc88c379573c9f0642d262b9ccc85fbccff4a
Fangjun Kuang <csukuangfj@gmail.com>
2023-04-04 13:59:58 +0800

    small fixes

--- cmake/onnxruntime.cmake
+++ cmake/onnxruntime.cmake
@@ -147,7 +147,7 @@ function(download_onnxruntime)
   endif()
 
   if(UNIX AND NOT APPLE)
-    file(GLOB onnxruntime_lib_files "${onnxruntime_SOURCE_DIR}/lib/lib*so*")
+    file(GLOB onnxruntime_lib_files "${onnxruntime_SOURCE_DIR}/lib/lib*")
   elseif(APPLE)
     file(GLOB onnxruntime_lib_files "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime.*.*dylib")
   elseif(WIN32)
