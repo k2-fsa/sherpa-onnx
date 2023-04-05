9ac747248bb66c54fdb7d32d15b3b14e8f3fd1db
Fangjun Kuang <csukuangfj@gmail.com>
2023-04-05 22:16:29 +0800

    Add SwiftUI demo project (#118)
    * Commit after creating the project
    
    * Add sherpa-onnx related files
    
    * copy and modify files from sherpa-ncnn
    
    * add app icon

---
+++ ios-swiftui/SherpaOnnx/SherpaOnnx/SherpaOnnxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SherpaOnnxApp.swift
+//  SherpaOnnx
+//
+//  Created by fangjun on 2023/4/5.
+//
+
+import SwiftUI
+
+@main
+struct SherpaOnnxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
