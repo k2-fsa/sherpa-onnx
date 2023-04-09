d781fcdeefbea58834167add758ebf38c75e9061
彭震东 <275331498@qq.com>
2023-04-09 23:28:10 +0800

    Use log probs for paraformer (#120)
    * Use log probs for paraformer
    
    * Fix

--- sherpa-onnx/csrc/offline-paraformer-greedy-search-decoder.cc
+++ sherpa-onnx/csrc/offline-paraformer-greedy-search-decoder.cc
@@ -4,28 +4,33 @@
 
 #include "sherpa-onnx/csrc/offline-paraformer-greedy-search-decoder.h"
 
+#include <algorithm>
 #include <vector>
 
 namespace sherpa_onnx {
 
 std::vector<OfflineParaformerDecoderResult>
-OfflineParaformerGreedySearchDecoder::Decode(Ort::Value /*log_probs*/,
-                                             Ort::Value token_num) {
-  std::vector<int64_t> shape = token_num.GetTensorTypeAndShapeInfo().GetShape();
+OfflineParaformerGreedySearchDecoder::Decode(Ort::Value log_probs,
+                                             Ort::Value /*token_num*/) {
+  std::vector<int64_t> shape = log_probs.GetTensorTypeAndShapeInfo().GetShape();
   int32_t batch_size = shape[0];
   int32_t num_tokens = shape[1];
+  int32_t vocab_size = shape[2];
 
   std::vector<OfflineParaformerDecoderResult> results(batch_size);
 
-  const int64_t *p = token_num.GetTensorData<int64_t>();
   for (int32_t i = 0; i != batch_size; ++i) {
+    const float *p =
+        log_probs.GetTensorData<float>() + i * num_tokens * vocab_size;
     for (int32_t k = 0; k != num_tokens; ++k) {
-      if (p[k] == eos_id_) break;
+      auto max_idx = static_cast<int64_t>(
+          std::distance(p, std::max_element(p, p + vocab_size)));
+      if (max_idx == eos_id_) break;
 
-      results[i].tokens.push_back(p[k]);
+      results[i].tokens.push_back(max_idx);
+
+      p += vocab_size;
     }
-
-    p += num_tokens;
   }
 
   return results;
