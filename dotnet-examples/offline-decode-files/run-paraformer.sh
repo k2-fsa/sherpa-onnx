72f6531619d48c5dd17cd3fc901ff731b897d720
Fangjun Kuang <csukuangfj@gmail.com>
2023-05-08 22:18:02 +0800

    fix a typo

--- dotnet-examples/offline-decode-files/run-paraformer.sh
+++ dotnet-examples/offline-decode-files/run-paraformer.sh
@@ -11,8 +11,7 @@ dotnet run \
   --tokens=./sherpa-onnx-paraformer-zh-2023-03-28/tokens.txt \
   --paraformer=./sherpa-onnx-paraformer-zh-2023-03-28/model.onnx \
   --num-threads=2 \
-  --files ./sherpa-onnx-zipformer-en-2023-04-01/test_wavs/0.wav \
-  ./sherpa-onnx-paraformer-zh-2023-03-28/test_wavs/0.wav \
+  --files ./sherpa-onnx-paraformer-zh-2023-03-28/test_wavs/0.wav \
   ./sherpa-onnx-paraformer-zh-2023-03-28/test_wavs/1.wav \
   ./sherpa-onnx-paraformer-zh-2023-03-28/test_wavs/2.wav \
   ./sherpa-onnx-paraformer-zh-2023-03-28/test_wavs/8k.wav
