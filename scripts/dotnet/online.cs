7969cf44ac3eb10c3a881cbb11df9cf0d4fcc4f7
Fangjun Kuang <csukuangfj@gmail.com>
2023-05-10 14:53:04 +0800

    Refactor C# code and support building nuget packages for cross-platforms (#144)

---
+++ scripts/dotnet/online.cs
@@ -0,0 +1,291 @@
+﻿/// Copyright (c)  2023  Xiaomi Corporation (authors: Fangjun Kuang)
+/// Copyright (c)  2023 by manyeyes
+
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System;
+
+namespace SherpaOnnx
+{
+  internal static class Dll
+  {
+    public const string Filename = "sherpa-onnx-c-api";
+  }
+
+  [StructLayout(LayoutKind.Sequential)]
+  public struct OnlineTransducerModelConfig
+  {
+    public OnlineTransducerModelConfig()
+    {
+      Encoder = "";
+      Decoder = "";
+      Joiner = "";
+      Tokens = "";
+      NumThreads = 1;
+      Debug = 0;
+    }
+    [MarshalAs(UnmanagedType.LPStr)]
+    public string Encoder;
+
+    [MarshalAs(UnmanagedType.LPStr)]
+    public string Decoder;
+
+    [MarshalAs(UnmanagedType.LPStr)]
+    public string Joiner;
+
+    [MarshalAs(UnmanagedType.LPStr)]
+    public string Tokens;
+
+    /// Number of threads used to run the neural network model
+    public int NumThreads;
+
+    /// true to print debug information of the model
+    public int Debug;
+  }
+
+  /// It expects 16 kHz 16-bit single channel wave format.
+  [StructLayout(LayoutKind.Sequential)]
+  public struct FeatureConfig
+  {
+    public FeatureConfig()
+    {
+      SampleRate = 16000;
+      FeatureDim = 80;
+    }
+    /// Sample rate of the input data. MUST match the one expected
+    /// by the model. For instance, it should be 16000 for models provided
+    /// by us.
+    public int SampleRate;
+
+    /// Feature dimension of the model.
+    /// For instance, it should be 80 for models provided by us.
+    public int FeatureDim;
+  }
+
+  [StructLayout(LayoutKind.Sequential)]
+  public struct OnlineRecognizerConfig
+  {
+    public OnlineRecognizerConfig()
+    {
+      FeatConfig = new FeatureConfig();
+      TransducerModelConfig = new OnlineTransducerModelConfig();
+      DecodingMethod = "greedy_search";
+      MaxActivePaths = 4;
+      EnableEndpoint = 0;
+      Rule1MinTrailingSilence = 1.2F;
+      Rule2MinTrailingSilence = 2.4F;
+      Rule3MinUtteranceLength = 20.0F;
+    }
+    public FeatureConfig FeatConfig;
+    public OnlineTransducerModelConfig TransducerModelConfig;
+
+    [MarshalAs(UnmanagedType.LPStr)]
+    public string DecodingMethod;
+
+    /// Used only when decoding_method is modified_beam_search
+    /// Example value: 4
+    public int MaxActivePaths;
+
+    /// 0 to disable endpoint detection.
+    /// A non-zero value to enable endpoint detection.
+    public int EnableEndpoint;
+
+    /// An endpoint is detected if trailing silence in seconds is larger than
+    /// this value even if nothing has been decoded.
+    /// Used only when enable_endpoint is not 0.
+    public float Rule1MinTrailingSilence;
+
+    /// An endpoint is detected if trailing silence in seconds is larger than
+    /// this value after something that is not blank has been decoded.
+    /// Used only when enable_endpoint is not 0.
+    public float Rule2MinTrailingSilence;
+
+    /// An endpoint is detected if the utterance in seconds is larger than
+    /// this value.
+    /// Used only when enable_endpoint is not 0.
+    public float Rule3MinUtteranceLength;
+  }
+
+  public class OnlineRecognizerResult
+  {
+    public OnlineRecognizerResult(IntPtr handle)
+    {
+      Impl impl = (Impl)Marshal.PtrToStructure(handle, typeof(Impl));
+      _text = Marshal.PtrToStringUTF8(impl.Text);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct Impl
+    {
+      public IntPtr Text;
+    }
+
+    private String _text;
+    public String Text => _text;
+  }
+
+  public class OnlineStream : IDisposable
+  {
+    public OnlineStream(IntPtr p)
+    {
+      _handle = new HandleRef(this, p);
+    }
+
+    public void AcceptWaveform(int sampleRate, float[] samples)
+    {
+      AcceptWaveform(Handle, sampleRate, samples, samples.Length);
+    }
+
+    public void InputFinished()
+    {
+      InputFinished(Handle);
+    }
+
+    ~OnlineStream()
+    {
+      Cleanup();
+    }
+
+    public void Dispose()
+    {
+      Cleanup();
+      // Prevent the object from being placed on the
+      // finalization queue
+      System.GC.SuppressFinalize(this);
+    }
+
+    private void Cleanup()
+    {
+      DestroyOnlineStream(Handle);
+
+      // Don't permit the handle to be used again.
+      _handle = new HandleRef(this, IntPtr.Zero);
+    }
+
+    private HandleRef _handle;
+    public IntPtr Handle => _handle.Handle;
+
+    [DllImport(Dll.Filename)]
+    private static extern void DestroyOnlineStream(IntPtr handle);
+
+    [DllImport(Dll.Filename)]
+    private static extern void AcceptWaveform(IntPtr handle, int sampleRate, float[] samples, int n);
+
+    [DllImport(Dll.Filename)]
+    private static extern void InputFinished(IntPtr handle);
+  }
+
+  // please see
+  // https://www.mono-project.com/docs/advanced/pinvoke/#gc-safe-pinvoke-code
+  // https://www.mono-project.com/docs/advanced/pinvoke/#properly-disposing-of-resources
+  public class OnlineRecognizer : IDisposable
+  {
+    public OnlineRecognizer(OnlineRecognizerConfig config)
+    {
+      IntPtr h = CreateOnlineRecognizer(ref config);
+      _handle = new HandleRef(this, h);
+    }
+
+    public OnlineStream CreateStream()
+    {
+      IntPtr p = CreateOnlineStream(_handle.Handle);
+      return new OnlineStream(p);
+    }
+
+    /// Return true if the passed stream is ready for decoding.
+    public bool IsReady(OnlineStream stream)
+    {
+      return IsReady(_handle.Handle, stream.Handle) != 0;
+    }
+
+    /// Return true if an endpoint is detected for this stream.
+    /// You probably need to invoke Reset(stream) when this method returns
+    /// true.
+    public bool IsEndpoint(OnlineStream stream)
+    {
+      return IsEndpoint(_handle.Handle, stream.Handle) != 0;
+    }
+
+    /// You have to ensure that IsReady(stream) returns true before
+    /// you call this method
+    public void Decode(OnlineStream stream)
+    {
+      Decode(_handle.Handle, stream.Handle);
+    }
+
+    // The caller should ensure all passed streams are ready for decoding.
+    public void Decode(IEnumerable<OnlineStream> streams)
+    {
+      IntPtr[] ptrs = streams.Select(s => s.Handle).ToArray();
+      Decode(_handle.Handle, ptrs, ptrs.Length);
+    }
+
+    public OnlineRecognizerResult GetResult(OnlineStream stream)
+    {
+      IntPtr h = GetResult(_handle.Handle, stream.Handle);
+      OnlineRecognizerResult result = new OnlineRecognizerResult(h);
+      DestroyResult(h);
+      return result;
+    }
+
+    /// When this method returns, IsEndpoint(stream) will return false.
+    public void Reset(OnlineStream stream)
+    {
+      Reset(_handle.Handle, stream.Handle);
+    }
+
+    public void Dispose()
+    {
+      Cleanup();
+      // Prevent the object from being placed on the
+      // finalization queue
+      System.GC.SuppressFinalize(this);
+    }
+
+    ~OnlineRecognizer()
+    {
+      Cleanup();
+    }
+
+    private void Cleanup()
+    {
+      DestroyOnlineRecognizer(_handle.Handle);
+
+      // Don't permit the handle to be used again.
+      _handle = new HandleRef(this, IntPtr.Zero);
+    }
+
+    private HandleRef _handle;
+
+    [DllImport(Dll.Filename)]
+    private static extern IntPtr CreateOnlineRecognizer(ref OnlineRecognizerConfig config);
+
+    [DllImport(Dll.Filename)]
+    private static extern void DestroyOnlineRecognizer(IntPtr handle);
+
+    [DllImport(Dll.Filename)]
+    private static extern IntPtr CreateOnlineStream(IntPtr handle);
+
+    [DllImport(Dll.Filename, EntryPoint = "IsOnlineStreamReady")]
+    private static extern int IsReady(IntPtr handle, IntPtr stream);
+
+    [DllImport(Dll.Filename, EntryPoint = "DecodeOnlineStream")]
+    private static extern void Decode(IntPtr handle, IntPtr stream);
+
+    [DllImport(Dll.Filename, EntryPoint = "DecodeMultipleOnlineStreams")]
+    private static extern void Decode(IntPtr handle, IntPtr[] streams, int n);
+
+    [DllImport(Dll.Filename, EntryPoint = "GetOnlineStreamResult")]
+    private static extern IntPtr GetResult(IntPtr handle, IntPtr stream);
+
+    [DllImport(Dll.Filename, EntryPoint = "DestroyOnlineRecognizerResult")]
+    private static extern void DestroyResult(IntPtr result);
+
+    [DllImport(Dll.Filename)]
+    private static extern void Reset(IntPtr handle, IntPtr stream);
+
+    [DllImport(Dll.Filename)]
+    private static extern int IsEndpoint(IntPtr handle, IntPtr stream);
+  }
+}
--- sherpa-onnx/c-api/c-api.cc
+++ sherpa-onnx/c-api/c-api.cc
@@ -10,10 +10,11 @@
 #include <vector>
 
 #include "sherpa-onnx/csrc/display.h"
+#include "sherpa-onnx/csrc/offline-recognizer.h"
 #include "sherpa-onnx/csrc/online-recognizer.h"
 
 struct SherpaOnnxOnlineRecognizer {
-  sherpa_onnx::OnlineRecognizer *impl;
+  std::unique_ptr<sherpa_onnx::OnlineRecognizer> impl;
 };
 
 struct SherpaOnnxOnlineStream {
@@ -56,14 +57,19 @@ SherpaOnnxOnlineRecognizer *CreateOnlineRecognizer(
   recognizer_config.endpoint_config.rule3.min_utterance_length =
       config->rule3_min_utterance_length;
 
+  if (config->model_config.debug) {
+    fprintf(stderr, "%s\n", recognizer_config.ToString().c_str());
+  }
+
   SherpaOnnxOnlineRecognizer *recognizer = new SherpaOnnxOnlineRecognizer;
-  recognizer->impl = new sherpa_onnx::OnlineRecognizer(recognizer_config);
+
+  recognizer->impl =
+      std::make_unique<sherpa_onnx::OnlineRecognizer>(recognizer_config);
 
   return recognizer;
 }
 
 void DestroyOnlineRecognizer(SherpaOnnxOnlineRecognizer *recognizer) {
-  delete recognizer->impl;
   delete recognizer;
 }
 
@@ -143,4 +149,117 @@ void DestroyDisplay(SherpaOnnxDisplay *display) { delete display; }
 
 void SherpaOnnxPrint(SherpaOnnxDisplay *display, int32_t idx, const char *s) {
   display->impl->Print(idx, s);
+}
+
+// ============================================================
+// For offline ASR (i.e., non-streaming ASR)
+// ============================================================
+//
+struct SherpaOnnxOfflineRecognizer {
+  std::unique_ptr<sherpa_onnx::OfflineRecognizer> impl;
+};
+
+struct SherpaOnnxOfflineStream {
+  std::unique_ptr<sherpa_onnx::OfflineStream> impl;
+  explicit SherpaOnnxOfflineStream(
+      std::unique_ptr<sherpa_onnx::OfflineStream> p)
+      : impl(std::move(p)) {}
+};
+
+SherpaOnnxOfflineRecognizer *CreateOfflineRecognizer(
+    const SherpaOnnxOfflineRecognizerConfig *config) {
+  sherpa_onnx::OfflineRecognizerConfig recognizer_config;
+
+  recognizer_config.feat_config.sampling_rate = config->feat_config.sample_rate;
+
+  recognizer_config.feat_config.feature_dim = config->feat_config.feature_dim;
+
+  recognizer_config.model_config.transducer.encoder_filename =
+      config->model_config.transducer.encoder;
+
+  recognizer_config.model_config.transducer.decoder_filename =
+      config->model_config.transducer.decoder;
+
+  recognizer_config.model_config.transducer.joiner_filename =
+      config->model_config.transducer.joiner;
+
+  recognizer_config.model_config.paraformer.model =
+      config->model_config.paraformer.model;
+
+  recognizer_config.model_config.nemo_ctc.model =
+      config->model_config.nemo_ctc.model;
+
+  recognizer_config.model_config.tokens = config->model_config.tokens;
+  recognizer_config.model_config.num_threads = config->model_config.num_threads;
+  recognizer_config.model_config.debug = config->model_config.debug;
+
+  recognizer_config.lm_config.model = config->lm_config.model;
+  recognizer_config.lm_config.scale = config->lm_config.scale;
+
+  recognizer_config.decoding_method = config->decoding_method;
+  recognizer_config.max_active_paths = config->max_active_paths;
+
+  if (config->model_config.debug) {
+    fprintf(stderr, "%s\n", recognizer_config.ToString().c_str());
+  }
+
+  SherpaOnnxOfflineRecognizer *recognizer = new SherpaOnnxOfflineRecognizer;
+
+  recognizer->impl =
+      std::make_unique<sherpa_onnx::OfflineRecognizer>(recognizer_config);
+
+  return recognizer;
+}
+
+void DestroyOfflineRecognizer(SherpaOnnxOfflineRecognizer *recognizer) {
+  delete recognizer;
+}
+
+SherpaOnnxOfflineStream *CreateOfflineStream(
+    const SherpaOnnxOfflineRecognizer *recognizer) {
+  SherpaOnnxOfflineStream *stream =
+      new SherpaOnnxOfflineStream(recognizer->impl->CreateStream());
+  return stream;
+}
+
+void DestoryOfflineStream(SherpaOnnxOfflineStream *stream) { delete stream; }
+
+void AcceptWaveformOffline(SherpaOnnxOfflineStream *stream, int32_t sample_rate,
+                           const float *samples, int32_t n) {
+  stream->impl->AcceptWaveform(sample_rate, samples, n);
+}
+
+void DecodeOfflineStream(SherpaOnnxOfflineRecognizer *recognizer,
+                         SherpaOnnxOfflineStream *stream) {
+  recognizer->impl->DecodeStream(stream->impl.get());
+}
+
+void DecodeMultipleOfflineStreams(SherpaOnnxOfflineRecognizer *recognizer,
+                                  SherpaOnnxOfflineStream **streams,
+                                  int32_t n) {
+  std::vector<sherpa_onnx::OfflineStream *> ss(n);
+  for (int32_t i = 0; i != n; ++i) {
+    ss[i] = streams[i]->impl.get();
+  }
+  recognizer->impl->DecodeStreams(ss.data(), n);
+}
+
+SherpaOnnxOfflineRecognizerResult *GetOfflineStreamResult(
+    SherpaOnnxOfflineStream *stream) {
+  const sherpa_onnx::OfflineRecognitionResult &result =
+      stream->impl->GetResult();
+  const auto &text = result.text;
+
+  auto r = new SherpaOnnxOfflineRecognizerResult;
+  r->text = new char[text.size() + 1];
+  std::copy(text.begin(), text.end(), const_cast<char *>(r->text));
+  const_cast<char *>(r->text)[text.size()] = 0;
+
+  return r;
+}
+
+void DestroyOfflineRecognizerResult(
+    const SherpaOnnxOfflineRecognizerResult *r) {
+  delete[] r->text;
+  delete r;
 }
