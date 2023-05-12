6741bd33ebf7cf90fc5dce48cf645fdb88bdbc77
Fangjun Kuang <csukuangfj@gmail.com>
2023-05-12 20:55:49 +0800

    avoid memory leak when calling onnxruntime API

--- sherpa-onnx/csrc/session.cc
+++ sherpa-onnx/csrc/session.cc
@@ -29,6 +29,8 @@ static Ort::SessionOptions GetSessionOptionsImpl(int32_t num_threads,
   sess_opts.SetIntraOpNumThreads(num_threads);
   sess_opts.SetInterOpNumThreads(num_threads);
 
+  const auto &api = Ort::GetApi();
+
   switch (p) {
     case Provider::kCPU:
       break;  // nothing to do for the CPU provider
@@ -43,8 +45,13 @@ static Ort::SessionOptions GetSessionOptionsImpl(int32_t num_threads,
     case Provider::kCoreML: {
 #if defined(__APPLE__)
       uint32_t coreml_flags = 0;
-      (void)OrtSessionOptionsAppendExecutionProvider_CoreML(sess_opts,
-                                                            coreml_flags);
+      OrtStatus *status = OrtSessionOptionsAppendExecutionProvider_CoreML(
+          sess_opts, coreml_flags);
+      if (!status) {
+        const char *msg = api.GetErrorMessage(status);
+        SHERPA_ONNX_LOGE("Failed to enable CoreML: %s. Fallback to cpu", msg);
+        api.ReleaseStatus(status);
+      }
 #else
       SHERPA_ONNX_LOGE("CoreML is for Apple only. Fallback to cpu!");
 #endif
@@ -55,9 +62,12 @@ static Ort::SessionOptions GetSessionOptionsImpl(int32_t num_threads,
       sess_opts.DisableMemPattern();
       sess_opts.SetExecutionMode(ORT_SEQUENTIAL);
       int32_t device_id = 0;
-      OrtStatus* onnx_status = OrtSessionOptionsAppendExecutionProvider_DML(sess_opts, device_id);
-      if (!onnx_status) {
-        fprintf(stderr, "failed to set directml\n");
+      OrtStatus *status =
+          OrtSessionOptionsAppendExecutionProvider_DML(sess_opts, device_id);
+      if (!status) {
+        const char *msg = api.GetErrorMessage(status);
+        SHERPA_ONNX_LOGE("Failed to enable DirectML: %s. Fallback to cpu", msg);
+        api.ReleaseStatus(status);
       }
 #else
       SHERPA_ONNX_LOGE("DirectML is for Windows only. Fallback to cpu!");
