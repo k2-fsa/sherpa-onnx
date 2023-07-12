06adbcae6a55d393e1d53ee56d3547dc04e1376a
Fangjun Kuang <csukuangfj@gmail.com>
2023-07-12 13:29:10 +0800

    Reformat with clang-format

--- mfc-examples/StreamingSpeechRecognition/StreamingSpeechRecognition.h
+++ mfc-examples/StreamingSpeechRecognition/StreamingSpeechRecognition.h
@@ -1,32 +1,31 @@
 
-// StreamingSpeechRecognition.h : main header file for the PROJECT_NAME application
+// StreamingSpeechRecognition.h : main header file for the PROJECT_NAME
+// application
 //
 
 #pragma once
 
 #ifndef __AFXWIN_H__
-	#error "include 'pch.h' before including this file for PCH"
+#error "include 'pch.h' before including this file for PCH"
 #endif
 
-#include "resource.h"		// main symbols
-
+#include "resource.h"  // main symbols
 
 // CStreamingSpeechRecognitionApp:
 // See StreamingSpeechRecognition.cpp for the implementation of this class
 //
 
-class CStreamingSpeechRecognitionApp : public CWinApp
-{
-public:
-	CStreamingSpeechRecognitionApp();
+class CStreamingSpeechRecognitionApp : public CWinApp {
+ public:
+  CStreamingSpeechRecognitionApp();
 
-// Overrides
-public:
-	virtual BOOL InitInstance();
+  // Overrides
+ public:
+  virtual BOOL InitInstance();
 
-// Implementation
+  // Implementation
 
-	DECLARE_MESSAGE_MAP()
+  DECLARE_MESSAGE_MAP()
 };
 
 extern CStreamingSpeechRecognitionApp theApp;
