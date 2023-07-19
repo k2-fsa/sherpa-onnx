e03fa378519ef8f5df0648fdbcd43540a4c0243b
Fangjun Kuang <csukuangfj@gmail.com>
2023-07-19 13:36:59 +0800

    First working version

--- go-api-examples/real-time-speech-recognition-from-microphone/go.mod
+++ go-api-examples/real-time-speech-recognition-from-microphone/go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/gordonklaus/portaudio v0.0.0-20230709114228-aafa478834f5
 	github.com/k2-fsa/sherpa-onnx-go/sherpa_onnx v0.0.0-20230718094340-597ada07dcf2
+	github.com/spf13/pflag v1.0.5
 )
 
 require (
--- go-api-examples/real-time-speech-recognition-from-microphone/main.go
+++ go-api-examples/real-time-speech-recognition-from-microphone/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"github.com/gordonklaus/portaudio"
 	sherpa "github.com/k2-fsa/sherpa-onnx-go/sherpa_onnx"
+	flag "github.com/spf13/pflag"
+	"strings"
+
+	"fmt"
 	"log"
 )
 
@@ -12,13 +16,12 @@ func main() {
 		log.Fatalf("Unable to initialize portaudio: %v\n", err)
 	}
 	defer portaudio.Terminate()
-	log.Println(portaudio.VersionText())
 
 	default_device, err := portaudio.DefaultInputDevice()
 	if err != nil {
 		log.Fatal("Failed to get default input device: %v\n", err)
 	}
-	log.Printf("Select default input device: %s\n", default_device.Name)
+	fmt.Printf("Select default input device: %s\n", default_device.Name)
 	param := portaudio.StreamParameters{}
 	param.Input.Device = default_device
 	param.Input.Channels = 1
@@ -30,24 +33,27 @@ func main() {
 
 	config := sherpa.OnlineRecognizerConfig{}
 	config.FeatConfig = sherpa.FeatureConfig{SampleRate: 16000, FeatureDim: 80}
-	config.ModelConfig = sherpa.OnlineTransducerModelConfig{
-		// Encoder:    "./sherpa-onnx-streaming-zipformer-en-2023-06-26/encoder-epoch-99-avg-1-chunk-16-left-128.onnx",
-		// Decoder:    "./sherpa-onnx-streaming-zipformer-en-2023-06-26/decoder-epoch-99-avg-1-chunk-16-left-128.onnx",
-		// Joiner:     "./sherpa-onnx-streaming-zipformer-en-2023-06-26/joiner-epoch-99-avg-1-chunk-16-left-128.onnx",
-		// Tokens:     "./sherpa-onnx-streaming-zipformer-en-2023-06-26/tokens.txt",
-		Encoder:    "./icefall-asr-zipformer-streaming-wenetspeech-20230615/exp/encoder-epoch-12-avg-4-chunk-16-left-128.onnx",
-		Decoder:    "./icefall-asr-zipformer-streaming-wenetspeech-20230615/exp/decoder-epoch-12-avg-4-chunk-16-left-128.onnx",
-		Joiner:     "./icefall-asr-zipformer-streaming-wenetspeech-20230615/exp/joiner-epoch-12-avg-4-chunk-16-left-128.onnx",
-		Tokens:     "./icefall-asr-zipformer-streaming-wenetspeech-20230615/data/lang_char/tokens.txt",
-		NumThreads: 1,
-		Provider:   "cpu",
-		Debug:      1,
-		ModelType:  "zipformer2",
-	}
-	config.DecodingMethod = "greedy_search"
-	config.EnableEndpoint = 0
 
+	flag.StringVar(&config.ModelConfig.Encoder, "encoder", "", "Path to the encoder model")
+	flag.StringVar(&config.ModelConfig.Decoder, "decoder", "", "Path to the decoder model")
+	flag.StringVar(&config.ModelConfig.Joiner, "joiner", "", "Path to the joiner model")
+	flag.StringVar(&config.ModelConfig.Tokens, "tokens", "", "Path to the tokens file")
+	flag.IntVar(&config.ModelConfig.NumThreads, "num-threads", 1, "Number of threads for computing")
+	flag.IntVar(&config.ModelConfig.Debug, "debug", 0, "Whether to show debug message")
+	flag.StringVar(&config.ModelConfig.ModelType, "model-type", "", "Optional. Used for loading the model in a faster way")
+	flag.StringVar(&config.ModelConfig.Provider, "provider", "cpu", "Provider to use")
+	flag.StringVar(&config.DecodingMethod, "decoding-method", "greedy_search", "Decoding method. Possible values: greedy_search, modified_beam_search")
+	flag.IntVar(&config.MaxActivePaths, "max-active-paths", 4, "Used only when --decoding-method is modified_beam_search")
+	flag.IntVar(&config.EnableEndpoint, "enable-endpoint", 1, "Whether to enable endpoint")
+	flag.Float32Var(&config.Rule1MinTrailingSilence, "rule1-min-trailing-silence", 2.4, "Threshold for rule1")
+	flag.Float32Var(&config.Rule2MinTrailingSilence, "rule2-min-trailing-silence", 1.2, "Threshold for rule2")
+	flag.Float32Var(&config.Rule3MinUtteranceLength, "rule3-min-utterance-length", 20, "Threshold for rule3")
+
+	flag.Parse()
+
+	log.Println("Initializing recognizer (may take several seconds)")
 	recognizer := sherpa.NewOnlineRecognizer(&config)
+	log.Println("Recognizer created!")
 	defer sherpa.DeleteOnlineRecognizer(recognizer)
 
 	stream := sherpa.NewOnlineStream(recognizer)
@@ -63,6 +69,12 @@ func main() {
 	defer s.Close()
 	chk(s.Start())
 
+	var last_text string
+
+	segment_idx := 0
+
+	fmt.Println("Started! Please speak")
+
 	for {
 		chk(s.Read())
 		stream.AcceptWaveform(int(param.SampleRate), samples)
@@ -70,7 +82,20 @@ func main() {
 		for recognizer.IsReady(stream) {
 			recognizer.Decode(stream)
 		}
-		log.Println(recognizer.GetResult(stream).Text)
+
+		text := recognizer.GetResult(stream).Text
+		if len(text) != 0 && last_text != text {
+			last_text = strings.ToLower(text)
+			fmt.Printf("\r%d: %s", segment_idx, last_text)
+		}
+
+		if recognizer.IsEndpoint(stream) {
+			if len(text) != 0 {
+				segment_idx++
+				fmt.Println()
+			}
+			recognizer.Reset(stream)
+		}
 	}
 
 	chk(s.Stop())
