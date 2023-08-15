243bd5c80b7b8e1291b988623cc07416c513818a
Fangjun Kuang <csukuangfj@gmail.com>
2023-08-15 22:50:07 +0800

    Fix kotlin API example

--- kotlin-api-examples/Main.kt
+++ kotlin-api-examples/Main.kt
@@ -11,10 +11,12 @@ fun main() {
     // please refer to
     // https://k2-fsa.github.io/sherpa/onnx/pretrained_models/index.html
     // to dowload pre-trained models
-    var modelConfig = OnlineTransducerModelConfig(
-        encoder = "./sherpa-onnx-streaming-zipformer-en-2023-02-21/encoder-epoch-99-avg-1.onnx",
-        decoder = "./sherpa-onnx-streaming-zipformer-en-2023-02-21/decoder-epoch-99-avg-1.onnx",
-        joiner = "./sherpa-onnx-streaming-zipformer-en-2023-02-21/joiner-epoch-99-avg-1.onnx",
+    var modelConfig = OnlineModelConfig(
+        transducer = OnlineTransducerModelConfig(
+            encoder = "./sherpa-onnx-streaming-zipformer-en-2023-02-21/encoder-epoch-99-avg-1.onnx",
+            decoder = "./sherpa-onnx-streaming-zipformer-en-2023-02-21/decoder-epoch-99-avg-1.onnx",
+            joiner = "./sherpa-onnx-streaming-zipformer-en-2023-02-21/joiner-epoch-99-avg-1.onnx",
+        ),
         tokens = "./sherpa-onnx-streaming-zipformer-en-2023-02-21/tokens.txt",
         numThreads = 1,
         debug = false,
@@ -41,19 +43,19 @@ fun main() {
     var objArray = WaveReader.readWaveFromFile(
         filename = "./sherpa-onnx-streaming-zipformer-en-2023-02-21/test_wavs/0.wav",
     )
-    var samples : FloatArray = objArray[0] as FloatArray
-    var sampleRate : Int = objArray[1] as Int
+    var samples: FloatArray = objArray[0] as FloatArray
+    var sampleRate: Int = objArray[1] as Int
 
-    model.acceptWaveform(samples, sampleRate=sampleRate)
+    model.acceptWaveform(samples, sampleRate = sampleRate)
     while (model.isReady()) {
-      model.decode()
+        model.decode()
     }
 
     var tailPaddings = FloatArray((sampleRate * 0.5).toInt()) // 0.5 seconds
-    model.acceptWaveform(tailPaddings, sampleRate=sampleRate)
+    model.acceptWaveform(tailPaddings, sampleRate = sampleRate)
     model.inputFinished()
     while (model.isReady()) {
-      model.decode()
+        model.decode()
     }
 
     println("results: ${model.text}")
