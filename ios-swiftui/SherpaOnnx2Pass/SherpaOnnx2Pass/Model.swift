6e4e1991e56126f8a040d2ef3d64bcadae55c95d
Fangjun Kuang <csukuangfj@gmail.com>
2023-09-11 16:29:40 +0800

    supporting building

---
+++ ios-swiftui/SherpaOnnx2Pass/SherpaOnnx2Pass/Model.swift
@@ -0,0 +1,53 @@
+import Foundation
+
+func getResource(_ forResource: String, _ ofType: String) -> String {
+  let path = Bundle.main.path(forResource: forResource, ofType: ofType)
+  precondition(
+    path != nil,
+    "\(forResource).\(ofType) does not exist!\n" + "Remember to change \n"
+      + "  Build Phases -> Copy Bundle Resources\n" + "to add it!"
+  )
+  return path!
+}
+/// Please refer to
+/// https://k2-fsa.github.io/sherpa/onnx/pretrained_models/index.html
+/// to download pre-trained models
+
+/// sherpa-onnx-streaming-zipformer-bilingual-zh-en-2023-02-20 (Bilingual, Chinese + English)
+/// https://k2-fsa.github.io/sherpa/onnx/pretrained_models/zipformer-transducer-models.html
+func getBilingualStreamZhEnZipformer20230220() -> SherpaOnnxOnlineModelConfig {
+  let encoder = getResource("encoder-epoch-99-avg-1", "onnx")
+  let decoder = getResource("decoder-epoch-99-avg-1", "onnx")
+  let joiner = getResource("joiner-epoch-99-avg-1", "onnx")
+  let tokens = getResource("tokens", "txt")
+
+  return sherpaOnnxOnlineModelConfig(
+    tokens: tokens,
+    transducer: sherpaOnnxOnlineTransducerModelConfig(
+      encoder: encoder,
+      decoder: decoder,
+      joiner: joiner),
+    numThreads: 2,
+    modelType: "zipformer"
+  )
+}
+
+// https://k2-fsa.github.io/sherpa/onnx/pretrained_models/online-paraformer/index.html
+func getBilingualStreamingZhEnParaformer() -> SherpaOnnxOnlineModelConfig {
+  let encoder = getResource("encoder.int8", "onnx")
+  let decoder = getResource("decoder.int8", "onnx")
+  let tokens = getResource("tokens", "txt")
+
+  return sherpaOnnxOnlineModelConfig(
+    tokens: tokens,
+    paraformer: sherpaOnnxOnlineParaformerModelConfig(
+      encoder: encoder,
+      decoder: decoder),
+    numThreads: 1,
+    modelType: "paraformer"
+  )
+}
+
+/// Please refer to
+/// https://k2-fsa.github.io/sherpa/onnx/pretrained_models/index.html
+/// to add more models if you need
