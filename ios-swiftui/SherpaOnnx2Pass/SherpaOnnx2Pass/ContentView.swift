1c44d2b8f4990804ec40260beabe9ab0ef919fa2
Fangjun Kuang <csukuangfj@gmail.com>
2023-09-11 16:06:13 +0800

    Add files after project creation

---
+++ ios-swiftui/SherpaOnnx2Pass/SherpaOnnx2Pass/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SherpaOnnx2Pass
+//
+//  Created by fangjun on 2023/9/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-swiftui/SherpaOnnx2Pass/SherpaOnnx2Pass/SherpaOnnx2PassApp.swift
@@ -0,0 +1,17 @@
+//
+//  SherpaOnnx2PassApp.swift
+//  SherpaOnnx2Pass
+//
+//  Created by fangjun on 2023/9/11.
+//
+
+import SwiftUI
+
+@main
+struct SherpaOnnx2PassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
