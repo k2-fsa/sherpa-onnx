af7793942b4a009605eac61a361995d6446dd156
Fangjun Kuang <csukuangfj@gmail.com>
2023-09-20 12:20:55 +0800

    small fixes

--- cmake/onnxruntime-linux-x86_64-static.cmake
+++ cmake/onnxruntime-linux-x86_64-static.cmake
@@ -64,3 +64,4 @@ file(GLOB onnxruntime_lib_files "${onnxruntime_SOURCE_DIR}/lib/lib*.a")
 set(onnxruntime_lib_files ${onnxruntime_lib_files} PARENT_SCOPE)
 
 message(STATUS "onnxruntime lib files: ${onnxruntime_lib_files}")
+install(FILES ${onnxruntime_lib_files} DESTINATION lib)
