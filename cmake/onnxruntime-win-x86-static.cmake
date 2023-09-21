c2acce147fbbb53db2efb309124c9ab61a8d101c
Fangjun Kuang <csukuangfj@gmail.com>
2023-09-21 14:20:33 +0800

    more fixes

--- cmake/onnxruntime-win-x86-static.cmake
+++ cmake/onnxruntime-win-x86-static.cmake
@@ -15,18 +15,18 @@ if(BUILD_SHARED_LIBS)
   message(FATAL_ERROR "This file is for building static libraries. BUILD_SHARED_LIBS: ${BUILD_SHARED_LIBS}")
 endif()
 
-set(onnxruntime_URL  "https://huggingface.co/csukuangfj/onnxruntime-libs/resolve/main/onnxruntime-win-x86-static-1.15.1.tar.bz2")
+set(onnxruntime_URL  "https://huggingface.co/csukuangfj/onnxruntime-libs/resolve/main/onnxruntime-win-x86-static-1.16.0.tar.bz2")
 set(onnxruntime_URL2 "")
-set(onnxruntime_HASH "SHA256=94d9a30976b5c4a5dff7508d00f141835916e5a36315d5f53be9b3edb85148b5")
+set(onnxruntime_HASH "SHA256=d03359dbd534aab621fb08b93929118ca5cfc35243071f96d2733c0cecb38616")
 
 # If you don't have access to the Internet,
 # please download onnxruntime to one of the following locations.
 # You can add more if you want.
 set(possible_file_locations
-  $ENV{HOME}/Downloads/onnxruntime-win-x86-static-1.15.1.tar.bz2
-  ${PROJECT_SOURCE_DIR}/onnxruntime-win-x86-static-1.15.1.tar.bz2
-  ${PROJECT_BINARY_DIR}/onnxruntime-win-x86-static-1.15.1.tar.bz2
-  /tmp/onnxruntime-win-x86-static-1.15.1.tar.bz2
+  $ENV{HOME}/Downloads/onnxruntime-win-x86-static-1.16.0.tar.bz2
+  ${PROJECT_SOURCE_DIR}/onnxruntime-win-x86-static-1.16.0.tar.bz2
+  ${PROJECT_BINARY_DIR}/onnxruntime-win-x86-static-1.16.0.tar.bz2
+  /tmp/onnxruntime-win-x86-static-1.16.0.tar.bz2
 )
 
 foreach(f IN LISTS possible_file_locations)
