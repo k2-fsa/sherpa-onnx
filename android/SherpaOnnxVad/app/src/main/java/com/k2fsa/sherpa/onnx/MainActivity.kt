d97d0d54e0069d635dacdb663536c1a194801ea0
Fangjun Kuang <csukuangfj@gmail.com>
2023-09-23 16:01:33 +0800

    add circles

--- android/SherpaOnnxVad/app/src/main/java/com/k2fsa/sherpa/onnx/MainActivity.kt
+++ android/SherpaOnnxVad/app/src/main/java/com/k2fsa/sherpa/onnx/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.k2fsa.sherpa.onnx
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recordButton: Button
+    private lateinit var circle: View
+
+    @Volatile
+    private var isRecording: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        circle= findViewById(R.id.powerCircle)
+
+        recordButton = findViewById(R.id.record_button)
+        recordButton.setOnClickListener { onclick() }
+    }
+
+    private fun onclick() {
+        if (!isRecording) {
+            isRecording = true
+            onVad(true)
+        } else {
+            isRecording= false
+            onVad(false)
+        }
+    }
+
+    private fun onVad(isSpeech: Boolean) {
+        if(isSpeech) {
+            circle.background = resources.getDrawable(R.drawable.green_circle)
+        } else {
+            circle.background = resources.getDrawable(R.drawable.gray_circle)
+        }
+
     }
 }
