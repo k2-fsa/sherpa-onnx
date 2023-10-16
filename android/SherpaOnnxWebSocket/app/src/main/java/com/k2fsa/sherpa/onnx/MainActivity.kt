7f7a2a842f4f6566bb04900e31d2f35498f3fbb6
longshiming <longshiming@greesoft.com>
2023-10-16 14:25:58 +0800

    update text show style

--- android/SherpaOnnxWebSocket/app/src/main/java/com/k2fsa/sherpa/onnx/MainActivity.kt
+++ android/SherpaOnnxWebSocket/app/src/main/java/com/k2fsa/sherpa/onnx/MainActivity.kt
@@ -15,6 +15,8 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.java_websocket.handshake.ServerHandshake
 import java.net.URI
 import java.net.URISyntaxException
@@ -45,11 +47,12 @@ class MainActivity : AppCompatActivity(), MyWebsocketClient.WebsocketClientCallb
     // since the AudioRecord.read(float[]) needs API level >= 23
     // but we are targeting API level >= 21
     private val audioFormat = AudioFormat.ENCODING_PCM_16BIT
-    private var idx: Int = 0
+    private var idx: Long = 0
     private var lastText: String = ""
 
     @Volatile
     private var isRecording: Boolean = false
+
     @Volatile
     private var isConnected: Boolean = false
 
@@ -85,6 +88,8 @@ class MainActivity : AppCompatActivity(), MyWebsocketClient.WebsocketClientCallb
 
         textView = findViewById(R.id.my_text)
         textView.movementMethod = ScrollingMovementMethod()
+
+        recordButton.isEnabled = false
 
         etUrl = findViewById(R.id.et_uri)
     }
@@ -131,6 +136,7 @@ class MainActivity : AppCompatActivity(), MyWebsocketClient.WebsocketClientCallb
             recordingThread = thread(true) {
                 processSamples()
             }
+            connectButton.isEnabled = false
             Log.i(TAG, "Started recording")
         } else {
             isRecording = false
@@ -138,6 +144,7 @@ class MainActivity : AppCompatActivity(), MyWebsocketClient.WebsocketClientCallb
             audioRecord!!.release()
             audioRecord = null
             recordButton.setText(R.string.start)
+            connectButton.isEnabled = true
             Log.i(TAG, "Stopped recording")
         }
     }
@@ -165,7 +172,7 @@ class MainActivity : AppCompatActivity(), MyWebsocketClient.WebsocketClientCallb
                 buffer.flip()
                 buffer.order(ByteOrder.LITTLE_ENDIAN)
 
-                if (isConnected){
+                if (isConnected) {
                     websocketClient?.send(buffer.array()) // send buf to server
                 }
 
@@ -201,14 +208,42 @@ class MainActivity : AppCompatActivity(), MyWebsocketClient.WebsocketClientCallb
         Log.i(TAG, "onOpen === >>")
         isConnected = true
         runOnUiThread {
+            recordButton.isEnabled = true
             connectButton.text = getString(R.string.disconnect)
         }
     }
 
+    private val gson = Gson()
+    private val recognitionText = hashMapOf<Long, String>()
+
+    private fun getDisplayResult(): String {
+        var i = 0
+        var ans = ""
+        for ((key,value) in recognitionText){
+            if (value == ""){
+                continue
+            }
+            ans += " $i : ${recognitionText[key]}\n"
+            i += 1
+        }
+        return ans
+
+    }
+
     override fun onMessage(message: String?) {
         Log.i(TAG, "onMessage === >> $message")
+        val speechContent = gson.fromJson<SpeechContent>(
+            message,
+            object : TypeToken<SpeechContent?>() {}.type
+        )
+
+        val text = speechContent.text
+        val segment = speechContent.segment
+        Log.i(TAG, "text === >> $text")
+
+        recognitionText[segment] = text
         runOnUiThread {
-            textView.text = message
+            textView.text = getDisplayResult()
         }
     }
 
@@ -216,14 +251,16 @@ class MainActivity : AppCompatActivity(), MyWebsocketClient.WebsocketClientCallb
         Log.i(TAG, "onClose === >> code$code reason$reason remote$remote")
         isConnected = false
         runOnUiThread {
+            recordButton.isEnabled = false
             connectButton.text = getString(R.string.connect)
+            textView.text = getString(R.string.hint)
         }
 
     }
 
     override fun onError(ex: Exception?) {
         Log.i(TAG, "onError === >> $ex")
-        runOnUiThread{
+        runOnUiThread {
             textView.text = "onError === >> $ex"
         }
 
---
+++ android/SherpaOnnxWebSocket/app/src/main/java/com/k2fsa/sherpa/onnx/SpeechContent.kt
@@ -0,0 +1,3 @@
+package com.k2fsa.sherpa.onnx
+
+data class SpeechContent(val text:String,val segment:Long)
