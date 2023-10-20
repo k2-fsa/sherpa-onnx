60da8b632611f4ce425971c87853ce4cb3f0fe7a
Fangjun Kuang <csukuangfj@gmail.com>
2023-10-20 16:41:06 +0800

    Commit after project creation

---
+++ mfc-examples/NonStreamingTextToSpeech/NonStreamingTextToSpeechDlg.h
@@ -0,0 +1,34 @@
+
+// NonStreamingTextToSpeechDlg.h : header file
+//
+
+#pragma once
+
+
+// CNonStreamingTextToSpeechDlg dialog
+class CNonStreamingTextToSpeechDlg : public CDialogEx
+{
+// Construction
+public:
+	CNonStreamingTextToSpeechDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_NONSTREAMINGTEXTTOSPEECH_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
