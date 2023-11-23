a4cff9474019d3a99842d926e57f80f11d3c4417
Fangjun Kuang <csukuangfj@gmail.com>
2023-11-23 19:37:05 +0800

    First working version

--- ios-swiftui/SherpaOnnxTts/SherpaOnnxTts/ContentView.swift
+++ ios-swiftui/SherpaOnnxTts/SherpaOnnxTts/ContentView.swift
@@ -4,15 +4,23 @@
 //
 //  Created by fangjun on 2023/11/23.
 //
+// Speech-to-text with Next-gen Kaldi on iOS without Internet connection
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     @State private var sid = "0"
     @State private var speed = 1.0
     @State private var text = ""
+    @State private var showAlert = false
+    @State var filename: URL = NSURL() as URL
+    @State var audioPlayer: AVAudioPlayer!
+
+    private var tts = createOfflineTts()
+
     var body: some View {
-        
+
         VStack(alignment: .leading) {
             HStack {
                 Spacer()
@@ -31,27 +39,47 @@ struct ContentView: View {
                     Text("Speech speed")
                 }
             }
-            
+
             Text("Please input your text below").padding([.trailing, .top, .bottom])
-            
+
             TextEditor(text: $text)
                 .font(.body)
                 .opacity(self.text.isEmpty ? 0.25 : 1)
                 .disableAutocorrection(true)
                 .border(Color.black)
-            
+
             Spacer()
             HStack {
                 Spacer()
                 Button(action: {
-                    text += "1"
+                    let speakerId = Int(self.sid) ?? 0
+                    let t = self.text.trimmingCharacters(in: .whitespacesAndNewlines)
+                    if t.isEmpty {
+                        self.showAlert = true
+                        return
+                    }
+
+                    let audio = tts.generate(text: t, sid: speakerId, speed: Float(self.speed))
+                    if self.filename.absoluteString.isEmpty {
+                        let tempDirectoryURL = NSURL.fileURL(withPath: NSTemporaryDirectory(), isDirectory: true)
+                        self.filename = tempDirectoryURL.appendingPathComponent("test.wav")
+                    }
+
+                    let ret = audio.save(filename: filename.path)
+
+                    self.audioPlayer = try! AVAudioPlayer(contentsOf: filename)
+                    self.audioPlayer.play()
                 }) {
                     Text("Generate")
+                }.alert(isPresented: $showAlert) {
+                    Alert(title: Text("Empty text"), message: Text("Please input your text before clicking the Generate button"))
                 }
                 Spacer()
-                Button(action:{}){
+                Button (action: {
+                    self.audioPlayer.play()
+                }) {
                     Text("Play")
-                }
+                }.disabled(filename.absoluteString.isEmpty)
                 Spacer()
             }
             Spacer()
