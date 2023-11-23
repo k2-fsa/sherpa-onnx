46c2e76a8c7cc047482dcad58eea2e91f16c8883
Fangjun Kuang <csukuangfj@gmail.com>
2023-11-23 10:24:21 +0800

    commit after project creation

---
+++ ios-swiftui/SherpaOnnxTts/SherpaOnnxTts/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SherpaOnnxTts
+//
+//  Created by fangjun on 2023/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
