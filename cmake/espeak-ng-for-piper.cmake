15d597149eab268389a138adf197daf2226aa453
Fangjun Kuang <csukuangfj@gmail.com>
2023-11-28 17:28:29 +0800

    fix include path for espeak-ng

--- cmake/espeak-ng-for-piper.cmake
+++ cmake/espeak-ng-for-piper.cmake
@@ -1,9 +1,9 @@
 function(download_espeak_ng_for_piper)
   include(FetchContent)
 
-  set(espeak_ng_URL  "https://github.com/csukuangfj/espeak-ng/archive/39669a6fa9309a4a7ba9284327484aa7b812d564.zip")
+  set(espeak_ng_URL  "https://github.com/csukuangfj/espeak-ng/archive/2b20e6b2c2aadc701fb7831c113aa9dbcb2dbc24.zip")
   set(espeak_ng_URL2 "")
-  set(espeak_ng_HASH "SHA256=f78864fc4413b4487dedd4a4587f98d6a8ad7225e196fe77dd7d7c5dcc42e53e")
+  set(espeak_ng_HASH "SHA256=5bd8b8a8a5550ad08050b4c622b4e5755dea487e0c08bfea035b24d173a91342")
 
   set(USE_ASYNC OFF CACHE BOOL "" FORCE)
   set(USE_MBROLA OFF CACHE BOOL "" FORCE)
@@ -18,11 +18,11 @@ function(download_espeak_ng_for_piper)
   # If you don't have access to the Internet,
   # please pre-download kaldi-decoder
   set(possible_file_locations
-    $ENV{HOME}/Downloads/espeak-ng-39669a6fa9309a4a7ba9284327484aa7b812d564.zip
-    ${PROJECT_SOURCE_DIR}/espeak-ng-39669a6fa9309a4a7ba9284327484aa7b812d564.zip
-    ${PROJECT_BINARY_DIR}/espeak-ng-39669a6fa9309a4a7ba9284327484aa7b812d564.zip
-    /tmp/espeak-ng-39669a6fa9309a4a7ba9284327484aa7b812d564.zip
-    /star-fj/fangjun/download/github/espeak-ng-39669a6fa9309a4a7ba9284327484aa7b812d564.zip
+    $ENV{HOME}/Downloads/espeak-ng-2b20e6b2c2aadc701fb7831c113aa9dbcb2dbc24.zip
+    ${PROJECT_SOURCE_DIR}/espeak-ng-2b20e6b2c2aadc701fb7831c113aa9dbcb2dbc24.zip
+    ${PROJECT_BINARY_DIR}/espeak-ng-2b20e6b2c2aadc701fb7831c113aa9dbcb2dbc24.zip
+    /tmp/espeak-ng-2b20e6b2c2aadc701fb7831c113aa9dbcb2dbc24.zip
+    /star-fj/fangjun/download/github/espeak-ng-2b20e6b2c2aadc701fb7831c113aa9dbcb2dbc24.zip
   )
 
   foreach(f IN LISTS possible_file_locations)
