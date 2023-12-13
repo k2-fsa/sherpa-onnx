9829d7c4d3f19ec5aa019ce1e6dedad6452a66d2
Fangjun Kuang <csukuangfj@gmail.com>
2023-12-13 15:40:07 +0800

    Add two GLaDOS TTS models (#481)

--- scripts/apk/generate-tts-apk-script.py
+++ scripts/apk/generate-tts-apk-script.py
@@ -79,6 +79,7 @@ def get_piper_models() -> List[TtsModel]:
         TtsModel(model_dir="vits-piper-en_GB-southern_english_female-low"),
         TtsModel(model_dir="vits-piper-en_GB-sweetbbak-amy"),
         TtsModel(model_dir="vits-piper-en_GB-vctk-medium"),
+        TtsModel(model_dir="vits-piper-en_US-glados"),
         TtsModel(model_dir="vits-piper-en_US-amy-low"),
         TtsModel(model_dir="vits-piper-en_US-amy-medium"),
         TtsModel(model_dir="vits-piper-en_US-arctic-medium"),
@@ -96,6 +97,7 @@ def get_piper_models() -> List[TtsModel]:
         TtsModel(model_dir="vits-piper-en_US-ryan-high"),
         TtsModel(model_dir="vits-piper-en_US-ryan-low"),
         TtsModel(model_dir="vits-piper-en_US-ryan-medium"),
+        TtsModel(model_dir="vits-piper-es-glados-medium"),
         TtsModel(model_dir="vits-piper-es_ES-carlfm-x_low"),
         TtsModel(model_dir="vits-piper-es_ES-davefx-medium"),
         TtsModel(model_dir="vits-piper-es_ES-mls_10246-low"),
@@ -158,7 +160,7 @@ def get_piper_models() -> List[TtsModel]:
     for m in models:
         m.data_dir = m.model_dir + "/" + "espeak-ng-data"
         m.model_name = m.model_dir[len("vits-piper-") :] + ".onnx"
-        m.lang = "en"
+        m.lang = m.model_dir.split("-")[2][:2]
 
     return models
 
