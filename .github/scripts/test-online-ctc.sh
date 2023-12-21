d4caf470078a2b7e027e7a09253260a9120d5bcc
Fangjun Kuang <csukuangfj@gmail.com>
2023-12-21 10:33:04 +0800

    test online zipformer2 CTC

--- .github/scripts/test-online-ctc.sh
+++ .github/scripts/test-online-ctc.sh
@@ -14,6 +14,25 @@ echo "PATH: $PATH"
 which $EXE
 
 log "------------------------------------------------------------"
+log "Run streaming Zipformer2 CTC                                "
+log "------------------------------------------------------------"
+
+url=https://github.com/k2-fsa/sherpa-onnx/releases/download/asr-models/sherpa-onnx-streaming-zipformer-ctc-multi-zh-hans-2023-12-13.tar.bz2
+repo=$(basename -s .tar.bz2 $url)
+wget -qq $url
+tar xvf $repo.tar.bz2
+rm $repo.tar.bz2
+
+time $EXE \
+  --debug=1 \
+  --zipformer2-ctc-model=$repo/ctc-epoch-20-avg-1-chunk-16-left-128.int8.onnx \
+  --tokens=$repo/tokens.txt \
+  $repo/test_wavs/DEV_T0000000000.wav \
+  $repo/test_wavs/DEV_T0000000001.wav \
+  $repo/test_wavs/DEV_T0000000002.wav
+
+
+log "------------------------------------------------------------"
 log "Run streaming Conformer CTC from WeNet"
 log "------------------------------------------------------------"
 wenet_models=(
