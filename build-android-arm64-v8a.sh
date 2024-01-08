47bcbe4c546ac1debc3bc79e5bb7fdfe9f241108
pkufool <wkang.pku@gmail.com>
2024-01-08 18:12:46 +0800

    Minor fixes

--- build-android-arm64-v8a.sh
+++ build-android-arm64-v8a.sh
@@ -89,7 +89,7 @@ cmake -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake" 
     -DANDROID_PLATFORM=android-21 ..
 
 # make VERBOSE=1 -j4
-make -j10
+make -j4
 make install/strip
 cp -fv android-onnxruntime-libs/jni/arm64-v8a/libonnxruntime.so install/lib
 rm -rf install/lib/pkgconfig
