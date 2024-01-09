c289a11c24d290c8d55f7169439de71e71a718d9
Fangjun Kuang <csukuangfj@gmail.com>
2024-01-09 09:40:38 +0800

    Release GIL for speaker embedding manager

--- sherpa-onnx/python/csrc/speaker-embedding-manager.cc
+++ sherpa-onnx/python/csrc/speaker-embedding-manager.cc
@@ -8,39 +8,42 @@
 #include <vector>
 
 #include "sherpa-onnx/csrc/speaker-embedding-manager.h"
-#include "sherpa-onnx/python/csrc/speaker-embedding-manager.h"
 
 namespace sherpa_onnx {
 
 void PybindSpeakerEmbeddingManager(py::module *m) {
   using PyClass = SpeakerEmbeddingManager;
   py::class_<PyClass>(*m, "SpeakerEmbeddingManager")
-      .def(py::init<int32_t>(), py::arg("dim"))
+      .def(py::init<int32_t>(), py::arg("dim"),
+           py::call_guard<py::gil_scoped_release>())
       .def(
           "add",
           [](const PyClass &self, const std::string &name,
              const std::vector<float> &v) -> bool {
             return self.Add(name, v.data());
           },
-          py::arg("name"), py::arg("v"))
+          py::arg("name"), py::arg("v"),
+          py::call_guard<py::gil_scoped_release>())
       .def(
           "remove",
           [](const PyClass &self, const std::string &name) -> bool {
             return self.Remove(name);
           },
-          py::arg("name"))
+          py::arg("name"), py::call_guard<py::gil_scoped_release>())
       .def(
           "search",
           [](const PyClass &self, const std::vector<float> &v, float threshold)
               -> std::string { return self.Search(v.data(), threshold); },
-          py::arg("v"), py::arg("threshold"))
+          py::arg("v"), py::arg("threshold"),
+          py::call_guard<py::gil_scoped_release>())
       .def(
           "verify",
           [](const PyClass &self, const std::string &name,
              const std::vector<float> &v, float threshold) -> bool {
             return self.Verify(name, v.data(), threshold);
           },
-          py::arg("name"), py::arg("v"), py::arg("threshold"));
+          py::arg("name"), py::arg("v"), py::arg("threshold"),
+          py::call_guard<py::gil_scoped_release>());
 }
 
 }  // namespace sherpa_onnx
