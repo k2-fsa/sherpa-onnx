b1c61cf372a93d37f51e271b36034b5cdf34049d
Chii Yeh <chiiyeh@gmail.com>
2024-01-25 09:35:20 +0800

    Shift blank penalty into the loop for greedy search

--- sherpa-onnx/csrc/offline-transducer-greedy-search-decoder.cc
+++ sherpa-onnx/csrc/offline-transducer-greedy-search-decoder.cc
@@ -47,11 +47,11 @@ OfflineTransducerGreedySearchDecoder::Decode(Ort::Value encoder_out,
     Ort::Value logit = model_->RunJoiner(std::move(cur_encoder_out),
                                          std::move(cur_decoder_out));
     float *p_logit = logit.GetTensorMutableData<float>();
-    if (blank_penalty_ > 0.0) {
-      p_logit[0] -= blank_penalty_; // assuming blank id is 0
-    }
     bool emitted = false;
     for (int32_t i = 0; i != n; ++i) {
+      if (blank_penalty_ > 0.0) {
+        p_logit[0] -= blank_penalty_; // assuming blank id is 0
+      }
       auto y = static_cast<int32_t>(std::distance(
           static_cast<const float *>(p_logit),
           std::max_element(static_cast<const float *>(p_logit),
