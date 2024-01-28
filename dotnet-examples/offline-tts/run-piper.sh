33a4e139a8ee52a77041154efe785e77b277ac0f
Fangjun Kuang <csukuangfj@gmail.com>
2024-01-28 22:57:11 +0800

    Add TTS demo for C# API

---
+++ dotnet-examples/offline-tts/run-piper.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ ! -f ./vits-piper-en_US-amy-low/en_US-amy-low.onnx ]; then
+  wget -qq https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-piper-en_US-amy-low.tar.bz2
+  tar xf vits-piper-en_US-amy-low.tar.bz2
+  rm vits-piper-en_US-amy-low.tar.bz2
+fi
+
+dotnet run \
+  --vits-model=./vits-piper-en_US-amy-low/en_US-amy-low.onnx \
+  --vits-tokens=./vits-piper-en_US-amy-low/tokens.txt \
+  --vits-data-dir=./vits-piper-en_US-amy-low/espeak-ng-data \
+  --debug=1 \
+  --output-filename=./amy.wav \
+  --text="This is a text to speech application in dotnet with Next Generation Kaldi"
+
