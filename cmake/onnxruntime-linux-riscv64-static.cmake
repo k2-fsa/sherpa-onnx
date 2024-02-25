7c70de7ece336b07aa6fdf85492dbc675365e7e4
Fangjun Kuang <csukuangfj@gmail.com>
2024-02-25 23:46:06 +0800

    Add CI test with qemu for riscv64

--- cmake/onnxruntime-linux-riscv64-static.cmake
+++ cmake/onnxruntime-linux-riscv64-static.cmake
@@ -1,4 +1,4 @@
-# Copyright (c)  2022-2023  Xiaomi Corporation
+# Copyright (c)  2022-2024  Xiaomi Corporation
 message(STATUS "CMAKE_SYSTEM_NAME: ${CMAKE_SYSTEM_NAME}")
 message(STATUS "CMAKE_SYSTEM_PROCESSOR: ${CMAKE_SYSTEM_PROCESSOR}")
 
