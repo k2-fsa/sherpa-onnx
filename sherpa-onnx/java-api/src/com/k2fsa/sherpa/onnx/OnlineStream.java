a61ecc61702d266f2ff407698d742634ebc42cf2
Fangjun Kuang <csukuangfj@gmail.com>
2024-02-26 11:28:24 +0800

    Reformat code

--- sherpa-onnx/java-api/src/com/k2fsa/sherpa/onnx/OnlineStream.java
+++ sherpa-onnx/java-api/src/com/k2fsa/sherpa/onnx/OnlineStream.java
@@ -4,83 +4,81 @@
 // Stream is used for feeding data to the asr engine
 package com.k2fsa.sherpa.onnx;
 
-import java.io.*;
-import java.util.*;
-
 public class OnlineStream {
-  private long ptr = 0; // this is the  stream ptr
+    private long ptr = 0; // this is the  stream ptr
 
-  private int sampleRate = 16000;
-  // assign ptr to this stream in construction
-  public OnlineStream(long ptr, int sampleRate) {
-    this.ptr = ptr;
-    this.sampleRate = sampleRate;
-  }
+    private int sampleRate = 16000;
 
-  public long getPtr() {
-    return ptr;
-  }
+    // assign ptr to this stream in construction
+    public OnlineStream(long ptr, int sampleRate) {
+        this.ptr = ptr;
+        this.sampleRate = sampleRate;
+    }
 
-  public void acceptWaveform(float[] samples) throws Exception {
-    if (this.ptr == 0) throw new Exception("null exception for stream ptr");
+    public static void loadSoLib(String soPath) {
+        // load .so lib from the path
+        System.load(soPath.trim()); // ("sherpa-onnx-jni-java");
+    }
 
-    // feed wave data to asr engine
-    acceptWaveform(this.ptr, this.sampleRate, samples);
-  }
+    public long getPtr() {
+        return ptr;
+    }
 
-  public void inputFinished() {
-    // add some tail padding
-    int padLen = (int) (this.sampleRate * 0.3); // 0.3 seconds at 16 kHz sample rate
-    float tailPaddings[] = new float[padLen]; // default value is 0
-    acceptWaveform(this.ptr, this.sampleRate, tailPaddings);
+    public void acceptWaveform(float[] samples) throws Exception {
+        if (this.ptr == 0) throw new Exception("null exception for stream ptr");
 
-    // tell the engine all data are feeded
-    inputFinished(this.ptr);
-  }
+        // feed wave data to asr engine
+        acceptWaveform(this.ptr, this.sampleRate, samples);
+    }
 
-  public static void loadSoLib(String soPath) {
-    // load .so lib from the path
-    System.load(soPath.trim()); // ("sherpa-onnx-jni-java");
-  }
+    public void inputFinished() {
+        // add some tail padding
+        int padLen = (int) (this.sampleRate * 0.3); // 0.3 seconds at 16 kHz sample rate
+        float[] tailPaddings = new float[padLen]; // default value is 0
+        acceptWaveform(this.ptr, this.sampleRate, tailPaddings);
 
-  public void release() {
-    // stream object must be release after used
-    if (this.ptr == 0) return;
-    deleteStream(this.ptr);
-    this.ptr = 0;
-  }
+        // tell the engine all data are feeded
+        inputFinished(this.ptr);
+    }
 
-  protected void finalize() throws Throwable {
-    release();
-  }
+    public void release() {
+        // stream object must be release after used
+        if (this.ptr == 0) return;
+        deleteStream(this.ptr);
+        this.ptr = 0;
+    }
 
-  public boolean isLastFrame() throws Exception {
-    if (this.ptr == 0) throw new Exception("null exception for stream ptr");
-    return isLastFrame(this.ptr);
-  }
+    protected void finalize() throws Throwable {
+        release();
+    }
 
-  public void reSet() throws Exception {
-    if (this.ptr == 0) throw new Exception("null exception for stream ptr");
-    reSet(this.ptr);
-  }
+    public boolean isLastFrame() throws Exception {
+        if (this.ptr == 0) throw new Exception("null exception for stream ptr");
+        return isLastFrame(this.ptr);
+    }
 
-  public int featureDim() throws Exception {
-    if (this.ptr == 0) throw new Exception("null exception for stream ptr");
-    return featureDim(this.ptr);
-  }
+    public void reSet() throws Exception {
+        if (this.ptr == 0) throw new Exception("null exception for stream ptr");
+        reSet(this.ptr);
+    }
 
-  // JNI interface libsherpa-onnx-jni.so
-  private native void acceptWaveform(long ptr, int sampleRate, float[] samples);
+    public int featureDim() throws Exception {
+        if (this.ptr == 0) throw new Exception("null exception for stream ptr");
+        return featureDim(this.ptr);
+    }
 
-  private native void inputFinished(long ptr);
+    // JNI interface libsherpa-onnx-jni.so
+    private native void acceptWaveform(long ptr, int sampleRate, float[] samples);
 
-  private native void deleteStream(long ptr);
+    private native void inputFinished(long ptr);
 
-  private native int numFramesReady(long ptr);
+    private native void deleteStream(long ptr);
 
-  private native boolean isLastFrame(long ptr);
+    private native int numFramesReady(long ptr);
 
-  private native void reSet(long ptr);
+    private native boolean isLastFrame(long ptr);
 
-  private native int featureDim(long ptr);
+    private native void reSet(long ptr);
+
+    private native int featureDim(long ptr);
 }
