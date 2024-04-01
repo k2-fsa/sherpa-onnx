41e0976223224e96b04af34b912fa63377160d49
yujinqiu <yujinqiuustb@gmail.com>
2024-04-01 16:41:19 +0800

    init language identification repo

---
+++ ios-swiftui/SherpaOnnxLangID/SherpaOnnxLangID/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SherpaOnnxLangID
+//
+//  Created by knight on 2024/4/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
