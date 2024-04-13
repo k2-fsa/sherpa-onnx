ac11c43b23da8ba46f60082f880b7525efbfd5d9
Fangjun Kuang <csukuangfj@gmail.com>
2024-04-13 11:13:37 +0800

    fix typos

--- .github/scripts/test-offline-punctuation.sh
+++ .github/scripts/test-offline-punctuation.sh
@@ -20,7 +20,7 @@ log "------------------------------------------------------------"
 curl -SL -O https://github.com/k2-fsa/sherpa-onnx/releases/download/punctuation-models/sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12.tar.bz2
 tar xvf sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12.tar.bz2
 rm sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12.tar.bz2
-repo=sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12.
+repo=sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12
 ls -lh $repo
 
 $EXE \
