b68447ec1c5edb6f3df8578bae942a07cd234d06
Fangjun Kuang <csukuangfj@gmail.com>
2024-04-14 18:50:50 +0800

    fix a typo

--- .github/scripts/test-c-api.sh
+++ .github/scripts/test-c-api.sh
@@ -18,8 +18,10 @@ log "------------------------------------------------------------"
 log "Test adding punctuations                                    "
 log "------------------------------------------------------------"
 
-curl -SL O https://github.com/k2-fsa/sherpa-onnx/releases/download/punctuation-models/sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12.tar.bz2
-tar xvf sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12.tar.bz2
+curl -SL -O https://github.com/k2-fsa/sherpa-onnx/releases/download/punctuation-models/sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12.tar.bz2
+ls -lh
+tar xf sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12.tar.bz2
+ls -lh sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12
 rm sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12.tar.bz2
 $PUNCT_EXE
 rm -rf sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12
