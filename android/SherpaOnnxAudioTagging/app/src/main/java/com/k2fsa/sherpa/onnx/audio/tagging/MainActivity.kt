bcd9e48150d8bcaf67b10d681615ba039255c776
Fangjun Kuang <csukuangfj@gmail.com>
2024-04-16 20:47:16 +0800

    Add Android demo for audio tagging (#776)
    See https://k2-fsa.github.io/sherpa/onnx/audio-tagging/apk.html

---
+++ android/SherpaOnnxAudioTagging/app/src/main/java/com/k2fsa/sherpa/onnx/audio/tagging/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.k2fsa.sherpa.onnx.audio.tagging
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import com.k2fsa.sherpa.onnx.audio.tagging.ui.theme.SherpaOnnxAudioTaggingTheme
+
+const val TAG = "sherpa-onnx"
+
+private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
+
+class MainActivity : ComponentActivity() {
+    private val permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContent {
+            AudioTaggingApp()
+        }
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
+        Tagger.initTagger(this.assets)
+    }
+
+    @Suppress("DEPRECATION")
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+
+        if (!permissionToRecordAccepted) {
+            Log.e(TAG, "Audio record is disallowed")
+            Toast.makeText(
+                this,
+                "This App needs access to the microphone",
+                Toast.LENGTH_SHORT
+            )
+                .show()
+            finish()
+        }
+        Log.i(TAG, "Audio record is permitted")
+    }
+}
+
+@Composable
+fun AudioTaggingApp() {
+    SherpaOnnxAudioTaggingTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Home()
+        }
+    }
+}
---
+++ android/SherpaOnnxAudioTagging/app/src/main/java/com/k2fsa/sherpa/onnx/audio/tagging/Tagger.kt
@@ -0,0 +1,25 @@
+package com.k2fsa.sherpa.onnx.audio.tagging
+
+import android.content.res.AssetManager
+import android.util.Log
+import com.k2fsa.sherpa.onnx.AudioTagging
+import com.k2fsa.sherpa.onnx.getAudioTaggingConfig
+
+object Tagger {
+    private var _tagger: AudioTagging? = null
+    val tagger: AudioTagging
+        get() {
+            return _tagger!!
+        }
+    fun initTagger(assetManager: AssetManager? = null) {
+        synchronized(this) {
+            if (_tagger != null) {
+                return
+            }
+
+            Log.i(TAG, "Initializing audio tagger")
+            val config = getAudioTaggingConfig(type = 0)!!
+            _tagger = AudioTagging(assetManager, config)
+        }
+    }
+}
