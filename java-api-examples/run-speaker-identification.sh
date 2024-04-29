afe6d06d575a6ae06770adb3c2ce6c60d4534309
Fangjun Kuang <csukuangfj@gmail.com>
2024-04-29 21:19:01 +0800

    fix a typo

--- java-api-examples/run-speaker-identification.sh
+++ java-api-examples/run-speaker-identification.sh
@@ -31,7 +31,7 @@ fi
 
 if [ ! -f ./sr-data/enroll/leijun-sr-1.wav ]; then
   curl -SL -o sr-data.tar.gz https://github.com/csukuangfj/sr-data/archive/refs/tags/v1.0.0.tar.gz
-  tar xvf -o sr-data.tar.gz
+  tar xvf sr-data.tar.gz
   mv sr-data-1.0.0 sr-data
 fi
 
