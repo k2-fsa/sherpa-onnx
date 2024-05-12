469b08bf419393af77e56f98aa6f2068f2925baa
Fangjun Kuang <csukuangfj@gmail.com>
2024-05-12 11:56:36 +0800

    small fixes

--- scripts/node-addon-api/test/test_asr_streaming_transducer.js
+++ scripts/node-addon-api/test/test_asr_streaming_transducer.js
@@ -35,10 +35,10 @@ console.log('Started')
 let start = performance.now();
 const stream = recognizer.createStream();
 const wave = sherpa_onnx.readWave(waveFilename);
-stream.acceptWaveform(wave.samples, wave.sampleRate);
+stream.acceptWaveform({samples: wave.samples, sampleRate: wave.sampleRate});
 
 const tailPadding = new Float32Array(wave.sampleRate * 0.4);
-stream.acceptWaveform(tailPadding, wave.sampleRate);
+stream.acceptWaveform({samples: tailPadding, sampleRate: wave.sampleRate});
 
 while (recognizer.isReady(stream)) {
   recognizer.decode(stream);
