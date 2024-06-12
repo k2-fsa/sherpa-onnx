a7f65c6020f7f6cb9be2e232e2d9622f31663e2b
Fangjun Kuang <csukuangfj@gmail.com>
2024-06-12 10:41:27 +0800

    Save speaker ID

--- android/SherpaOnnxTtsEngine/app/src/main/java/com/k2fsa/sherpa/onnx/tts/engine/PreferencesHelper.kt
+++ android/SherpaOnnxTtsEngine/app/src/main/java/com/k2fsa/sherpa/onnx/tts/engine/PreferencesHelper.kt
@@ -5,6 +5,7 @@ class PreferenceHelper(context: Context) {
 
     private val PREFS_NAME = "com.k2fsa.sherpa.onnx.tts.engine"
     private val SPEED_KEY = "speed"
+    private val SID_KEY = "speaker_id"
 
     private val sharedPreferences: SharedPreferences =
         context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
@@ -17,5 +18,15 @@ class PreferenceHelper(context: Context) {
 
     fun getSpeed(): Float {
         return sharedPreferences.getFloat(SPEED_KEY, 1.0f)
+    }
+
+    fun setSid(value: Int) {
+        val editor = sharedPreferences.edit()
+        editor.putInt(SID_KEY, value)
+        editor.apply()
+    }
+
+    fun getSid(): Int {
+        return sharedPreferences.getInt(SID_KEY, 0)
     }
 }
