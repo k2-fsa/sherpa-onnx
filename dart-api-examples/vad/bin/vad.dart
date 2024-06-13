f9d7dc7422628f3f3a26d6ee3a895b1c3906b746
Fangjun Kuang <csukuangfj@gmail.com>
2024-06-13 16:05:13 +0800

    commit after creating dart-api vad examples

---
+++ dart-api-examples/vad/bin/vad.dart
@@ -0,0 +1,5 @@
+import 'package:vad/vad.dart' as vad;
+
+void main(List<String> arguments) {
+  print('Hello world: ${vad.calculate()}!');
+}
---
+++ dart-api-examples/vad/test/vad_test.dart
@@ -0,0 +1,8 @@
+import 'package:vad/vad.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
