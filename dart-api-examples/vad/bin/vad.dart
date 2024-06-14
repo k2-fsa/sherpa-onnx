9eb1b828eb7f283a37ce852d6b0f9d3cab58c415
Fangjun Kuang <csukuangfj@gmail.com>
2024-06-14 15:21:59 +0800

    refactor vad

--- dart-api-examples/vad/bin/vad.dart
+++ dart-api-examples/vad/bin/vad.dart
@@ -5,30 +5,7 @@ import 'dart:typed_data';
 import 'package:args/args.dart';
 import 'package:path/path.dart' as p;
 import 'package:sherpa_onnx/sherpa_onnx.dart' as sherpa_onnx;
-
-Future<void> initSherpaOnnx() async {
-  var uri = await Isolate.resolvePackageUri(
-      Uri.parse('package:sherpa_onnx/sherpa_onnx.dart'));
-
-  if (uri == null) {
-    print('File not found');
-    exit(1);
-  }
-  String platform = '';
-  if (Platform.isMacOS) {
-    platform = 'macos';
-  } else if (Platform.isLinux) {
-    platform = 'linux';
-  } else if (Platform.isWindows) {
-    platform = 'windows';
-  } else {
-    throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
-  }
-
-  final libPath = p.join(p.dirname(p.fromUri(uri)), '..', platform);
-  print('libPath: ${libPath}');
-  sherpa_onnx.initBindings(libPath);
-}
+import './init.dart';
 
 void main(List<String> arguments) async {
   await initSherpaOnnx();
@@ -37,6 +14,7 @@ void main(List<String> arguments) async {
     ..addOption('silero-vad', help: 'Path to silero_vad.onnx')
     ..addOption('input-wav', help: 'Path to input.wav')
     ..addOption('output-wav', help: 'Path to output.wav');
+
   final res = parser.parse(arguments);
   if (res['silero-vad'] == null ||
       res['input-wav'] == null ||
@@ -54,6 +32,7 @@ void main(List<String> arguments) async {
     minSilenceDuration: 0.25,
     minSpeechDuration: 0.5,
   );
+
   final config = sherpa_onnx.VadModelConfig(
     sileroVad: sileroVadConfig,
     numThreads: 1,
@@ -90,5 +69,6 @@ void main(List<String> arguments) async {
   final s = Float32List.fromList(allSamples.expand((x) => x).toList());
   sherpa_onnx.writeWave(
       filename: outputWav, samples: s, sampleRate: waveData.sampleRate);
+
   print('Saved to ${outputWav}');
 }
