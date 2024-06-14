382be39a770d242b9af9ef61a661221a19373ac7
Fangjun Kuang <csukuangfj@gmail.com>
2024-06-14 17:26:02 +0800

    VAD with paraformer example

--- .github/scripts/test-dart.sh
+++ .github/scripts/test-dart.sh
@@ -6,6 +6,10 @@ cd dart-api-examples
 
 pushd non-streaming-asr
 
+echo '----------VAD with paraformer----------'
+./run-vad-with-paraformer.sh
+rm -rf sherpa-onnx-*
+
 echo '----------NeMo transducer----------'
 ./run-nemo-transducer.sh
 rm -rf sherpa-onnx-*
