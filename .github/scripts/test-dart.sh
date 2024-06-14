f4df0f2204281564e0cc76167e7eb56c14a51cab
Fangjun Kuang <csukuangfj@gmail.com>
2024-06-14 17:05:03 +0800

    NeMo CTC example

--- .github/scripts/test-dart.sh
+++ .github/scripts/test-dart.sh
@@ -6,6 +6,10 @@ cd dart-api-examples
 
 pushd non-streaming-asr
 
+echo '----------NeMo CTC----------'
+./run-nemo-ctc.sh
+rm -rf sherpa-onnx-*
+
 echo '----------TeleSpeech CTC----------'
 ./run-telespeech-ctc.sh
 rm -rf sherpa-onnx-*
