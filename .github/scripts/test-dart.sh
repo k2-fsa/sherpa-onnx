c3e93fa5c3d90a8cbd247e6bf97ccc306a2029d7
Fangjun Kuang <csukuangfj@gmail.com>
2024-06-14 16:29:27 +0800

    telespeech ctc example

--- .github/scripts/test-dart.sh
+++ .github/scripts/test-dart.sh
@@ -5,18 +5,27 @@ set -ex
 cd dart-api-examples
 
 pushd non-streaming-asr
+
+echo '----------TeleSpeech CTC----------'
+./run-telespeech-ctc.sh
+rm -rf sherpa-onnx-*
+
 echo '----------paraformer----------'
 ./run-paraformer.sh
+rm -rf sherpa-onnx-*
 
 echo '----------whisper----------'
 ./run-whisper.sh
+rm -rf sherpa-onnx-*
 
 echo '----------zipformer transducer----------'
 ./run-zipformer-transducer.sh
+rm -rf sherpa-onnx-*
 
 popd
 
 pushd vad
 ./run.sh
+rm *.onnx
 popd
 
