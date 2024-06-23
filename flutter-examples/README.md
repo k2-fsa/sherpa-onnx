169c9bf627a03d56c17d69829e3d51cc307715bd
Fangjun Kuang <csukuangfj@gmail.com>
2024-06-23 13:29:13 +0800

    Flutter demo for real-time speech recognition (#1042)

---
+++ flutter-examples/README.md
@@ -0,0 +1,10 @@
+# Introduction
+
+This directory contains flutter examples of `sherpa-onnx`.
+
+# Ways to create an example
+```bash
+flutter create --platforms windows,macos streaming_asr
+cd streaming_asr
+flutter pub get
+```
---
+++ flutter-examples/streaming_asr/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <record_windows/record_windows_plugin_c_api.h>
+#include <url_launcher_windows/url_launcher_windows.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  RecordWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("RecordWindowsPluginCApi"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
+}
