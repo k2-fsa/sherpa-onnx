e7d61c0161712c4c0af2b578a9a2dd9cff073715
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-10 17:57:40 +0800

    Begin to add keyword spotting for C#

---
+++ scripts/dotnet/KeywordSpotterConfig.cs
@@ -0,0 +1,32 @@
+/// Copyright (c)  2024  Xiaomi Corporation
+
+using System.Runtime.InteropServices;
+
+namespace SherpaOnnx
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct KeywordSpotterConfig
+    {
+        public KeywordSpotterConfig()
+        {
+            FeatConfig = new FeatureConfig();
+            ModelConfig = new OnlineModelConfig();
+
+            MaxActivePaths = 4;
+            NumTrailingBlanks = 1;
+            KeywordsScore = 1.0F;
+            KeywordsThreshold = 0.25F;
+            KeywordsFile = "";
+        }
+        public FeatureConfig FeatConfig;
+        public OnlineModelConfig ModelConfig;
+
+        public int MaxActivePaths;
+        public int NumTrailingBlanks;
+        public float KeywordsScore;
+        public float KeywordsThreshold;
+
+        [MarshalAs(UnmanagedType.LPStr)]
+        public string KeywordsFile;
+    }
+}
