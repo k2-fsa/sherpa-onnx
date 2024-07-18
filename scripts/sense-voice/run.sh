3bae5c3fe562b68f8f72f0ec8bfb54dc5c315e1f
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-18 12:12:44 +0800

    test exported sense voice models (#1147)

--- scripts/sense-voice/run.sh
+++ scripts/sense-voice/run.sh
@@ -35,3 +35,28 @@ echo "pwd: $PWD"
 ./show-info.py
 
 ls -lh
+
+# Download test wavs
+curl -SL -O https://huggingface.co/csukuangfj/sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/resolve/main/test_wavs/en.wav
+curl -SL -O https://huggingface.co/csukuangfj/sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/resolve/main/test_wavs/zh.wav
+curl -SL -O https://huggingface.co/csukuangfj/sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/resolve/main/test_wavs/ja.wav
+curl -SL -O https://huggingface.co/csukuangfj/sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/resolve/main/test_wavs/ko.wav
+curl -SL -O https://huggingface.co/csukuangfj/sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/resolve/main/test_wavs/yue.wav
+
+for m in model.onnx model.int8.onnx; do
+  for w in en zh ja ko yue; do
+    echo "----------test $m $w.wav----------"
+
+    echo "without inverse text normalization, lang auto"
+    ./test.py --model $m --tokens ./tokens.txt --wav $w.wav --use-itn 0
+
+    echo "with inverse text normalization, lang auto"
+    ./test.py --model $m --tokens ./tokens.txt --wav $w.wav --use-itn 1
+
+    echo "without inverse text normalization, lang $w"
+    ./test.py --model $m --tokens ./tokens.txt --wav $w.wav --use-itn 0 --lang $w
+
+    echo "with inverse text normalization, lang $w"
+    ./test.py --model $m --tokens ./tokens.txt --wav $w.wav --use-itn 1 --lang $w
+  done
+done
