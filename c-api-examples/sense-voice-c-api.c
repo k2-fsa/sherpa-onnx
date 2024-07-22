3aa1963dedd4f11185dc47180f8b5a0731be0699
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-22 15:31:41 +0800

    Add C API examples for SenseVoice

--- c-api-examples/sense-voice-c-api.c
+++ c-api-examples/sense-voice-c-api.c
@@ -1,16 +1,14 @@
-// c-api-examples/offline-stt-c-api.c
+// c-api-examples/sense-voice-c-api.c
 //
 // Copyright (c)  2024  Xiaomi Corporation
 
-// We assume you have pre-downloaded the whisper multi-lingual models
-// from https://github.com/k2-fsa/sherpa-onnx/releases/tag/asr-models
-// An example command to download the "tiny" whisper model is given below:
 //
+// This file demonstrates how to use SenseVoice with sherpa-onnx's C API.
 // clang-format off
 //
-// wget https://github.com/k2-fsa/sherpa-onnx/releases/download/asr-models/sherpa-onnx-whisper-tiny.tar.bz2
-// tar xvf sherpa-onnx-whisper-tiny.tar.bz2
-// rm sherpa-onnx-whisper-tiny.tar.bz2
+// wget https://github.com/k2-fsa/sherpa-onnx/releases/download/asr-models/sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17.tar.bz2
+// tar xvf sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17.tar.bz2
+// rm sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17.tar.bz2
 //
 // clang-format on
 
@@ -23,12 +21,15 @@
 int32_t main() {
   // You can find more test waves from
   // https://hf-mirror.com/spaces/k2-fsa/spoken-language-identification/tree/main/test_wavs
-  const char *wav_filename = "./sherpa-onnx-whisper-tiny/test_wavs/0.wav";
-  const char *encoder_filename = "sherpa-onnx-whisper-tiny/tiny-encoder.onnx";
-  const char *decoder_filename = "sherpa-onnx-whisper-tiny/tiny-decoder.onnx";
-  const char *tokens_filename = "sherpa-onnx-whisper-tiny/tiny-tokens.txt";
-  const char *language = "en";
+  const char *wav_filename =
+      "./sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/test_wavs/en.wav";
+  const char *model_filename =
+      "./sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/model.int8.onnx";
+  const char *tokens_filename =
+      "./sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/tokens.txt";
+  const char *language = "auto";
   const char *provider = "cpu";
+  int32_t use_inverse_text_normalization = 1;
 
   const SherpaOnnxWave *wave = SherpaOnnxReadWave(wav_filename);
   if (wave == NULL) {
@@ -36,14 +37,11 @@ int32_t main() {
     return -1;
   }
 
-  // Whisper config
-  SherpaOnnxOfflineWhisperModelConfig whisper_config;
-  memset(&whisper_config, 0, sizeof(whisper_config));
-  whisper_config.decoder = decoder_filename;
-  whisper_config.encoder = encoder_filename;
-  whisper_config.language = language;
-  whisper_config.tail_paddings = 0;
-  whisper_config.task = "transcribe";
+  SherpaOnnxOfflineSenseVoiceModelConfig sense_voice_config;
+  memset(&sense_voice_config, 0, sizeof(sense_voice_config));
+  sense_voice_config.model = model_filename;
+  sense_voice_config.language = language;
+  sense_voice_config.use_itn = use_inverse_text_normalization;
 
   // Offline model config
   SherpaOnnxOfflineModelConfig offline_model_config;
@@ -52,7 +50,7 @@ int32_t main() {
   offline_model_config.num_threads = 1;
   offline_model_config.provider = provider;
   offline_model_config.tokens = tokens_filename;
-  offline_model_config.whisper = whisper_config;
+  offline_model_config.sense_voice = sense_voice_config;
 
   // Recognizer config
   SherpaOnnxOfflineRecognizerConfig recognizer_config;
