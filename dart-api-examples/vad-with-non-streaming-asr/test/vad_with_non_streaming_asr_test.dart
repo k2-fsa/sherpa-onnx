b9e9e1000cf37390fc223e85168c4caf530c19af
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-28 21:59:36 +0800

    remove unused files

--- dart-api-examples/vad-with-non-streaming-asr/test/vad_with_non_streaming_asr_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:vad_with_non_streaming_asr/vad_with_non_streaming_asr.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
