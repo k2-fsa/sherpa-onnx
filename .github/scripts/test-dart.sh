9ce4865ee232ae9263e401a4cacc8762c33b3555
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-28 22:47:55 +0800

    add more examples for zipformer transducer and telespeech ctc

--- .github/scripts/test-dart.sh
+++ .github/scripts/test-dart.sh
@@ -5,17 +5,30 @@ set -ex
 cd dart-api-examples
 
 pushd vad-with-non-streaming-asr
+echo '----------TeleSpeech CTC----------'
+./run-telespeech-ctc.sh
+rm -rf sherpa-onnx-*
+
+echo "----zipformer transducer----"
+./run-zipformer-transducer.sh
+rm -rf sherpa-onnx-*
+
 echo "----whisper----"
 ./run-whisper.sh
+rm -rf sherpa-onnx-*
 
 echo "----paraformer----"
 ./run-paraformer.sh
+rm -rf sherpa-onnx-*
 
 echo "----SenseVoice zh----"
 ./run-sense-voice-zh.sh
+rm -rf sherpa-onnx-*
 
 echo "----SenseVoice en----"
 ./run-sense-voice-en.sh
+rm -rf sherpa-onnx-*
+
 popd
 
 pushd keyword-spotter
