ad4f0216437dc5c078de2148a3c18e7ada63f796
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-29 12:12:02 +0800

    begin to example about adding puncutations with Dart API

---
+++ dart-api-examples/add-punctuations/bin/add_punctuations.dart
@@ -0,0 +1,5 @@
+import 'package:add_punctuations/add_punctuations.dart' as add_punctuations;
+
+void main(List<String> arguments) {
+  print('Hello world: ${add_punctuations.calculate()}!');
+}
---
+++ dart-api-examples/add-punctuations/test/add_punctuations_test.dart
@@ -0,0 +1,8 @@
+import 'package:add_punctuations/add_punctuations.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
