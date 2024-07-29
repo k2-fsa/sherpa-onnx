86c0a8067789f8843284f9491050db33dd2b1041
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-29 10:39:42 +0800

    begin to add audio tagging example for Dart API

---
+++ dart-api-examples/audio-tagging/bin/audio_tagging.dart
@@ -0,0 +1,5 @@
+import 'package:audio_tagging/audio_tagging.dart' as audio_tagging;
+
+void main(List<String> arguments) {
+  print('Hello world: ${audio_tagging.calculate()}!');
+}
---
+++ dart-api-examples/audio-tagging/test/audio_tagging_test.dart
@@ -0,0 +1,8 @@
+import 'package:audio_tagging/audio_tagging.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
