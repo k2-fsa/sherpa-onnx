1e1fabd6b4a841de656cc1fdf816b431d204de1d
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-29 18:14:08 +0800

    small fixes

--- dotnet-examples/offline-decode-files/run-whisper.sh
+++ dotnet-examples/offline-decode-files/run-whisper.sh
@@ -5,12 +5,17 @@ set -ex
 git lfs install
 
 git clone https://huggingface.co/csukuangfj/sherpa-onnx-whisper-large-v3
+
 ls -lh sherpa-onnx-whisper-large-v3
+cp -v sherpa-onnx-whisper-large-v3/*.onnx .
+cp -v sherpa-onnx-whisper-large-v3/*.weights .
+ls -lh
+
 
 dotnet run \
   --num-threads=2 \
-  --whisper-encoder=./sherpa-onnx-whisper-large-v3/large-v3-encoder.int8.onnx \
-  --whisper-decoder=./sherpa-onnx-whisper-large-v3/large-v3-decoder.int8.onnx \
+  --whisper-encoder=./large-v3-encoder.int8.onnx \
+  --whisper-decoder=./large-v3-decoder.int8.onnx \
   --tokens=./sherpa-onnx-whisper-large-v3/large-v3-tokens.txt \
   --files ./sherpa-onnx-whisper-large-v3/test_wavs/0.wav \
   ./sherpa-onnx-whisper-large-v3/test_wavs/1.wav \
@@ -18,8 +23,8 @@ dotnet run \
 
 dotnet run \
   --num-threads=2 \
-  --whisper-encoder=./sherpa-onnx-whisper-large-v3/large-v3-encoder.onnx \
-  --whisper-decoder=./sherpa-onnx-whisper-large-v3/large-v3-decoder.onnx \
+  --whisper-encoder=./large-v3-encoder.onnx \
+  --whisper-decoder=./large-v3-decoder.onnx \
   --tokens=./sherpa-onnx-whisper-large-v3/large-v3-tokens.txt \
   --files ./sherpa-onnx-whisper-large-v3/test_wavs/0.wav \
   ./sherpa-onnx-whisper-large-v3/test_wavs/1.wav \
