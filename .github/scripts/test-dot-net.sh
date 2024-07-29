06fd50f5368d69db6b6905b14dfa17aca95075f0
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-29 20:49:38 +0800

    Add test about whisper large-v3 for .Net (#1187)

--- .github/scripts/test-dot-net.sh
+++ .github/scripts/test-dot-net.sh
@@ -4,38 +4,68 @@ cd dotnet-examples/
 
 cd ./offline-decode-files
 ./run-sense-voice-ctc.sh
+rm -rf sherpa-onnx-*
+
 ./run-paraformer-itn.sh
+rm -rf sherpa-onnx-*
+
 ./run-telespeech-ctc.sh
+rm -rf sherpa-onnx-*
+
 ./run-nemo-ctc.sh
+rm -rf sherpa-onnx-*
+
 ./run-paraformer.sh
+rm -rf sherpa-onnx-*
+
 ./run-zipformer.sh
+rm -rf sherpa-onnx-*
+
 ./run-hotwords.sh
+rm -rf sherpa-onnx-*
+
 ./run-whisper.sh
+rm -rf sherpa-onnx-*
+
+./run-whisper-large-v3.sh
+rm -rf sherpa-onnx-*
+
 ./run-tdnn-yesno.sh
+rm -rf sherpa-onnx-*
 
 cd ../keyword-spotting-from-files
 ./run.sh
 
 cd ../online-decode-files
 ./run-transducer-itn.sh
+rm -rf sherpa-onnx-*
+
 ./run-zipformer2-ctc.sh
+rm -rf sherpa-onnx-*
+
 ./run-transducer.sh
+rm -rf sherpa-onnx-*
+
 ./run-paraformer.sh
+rm -rf sherpa-onnx-*
 
 cd ../vad-non-streaming-asr-paraformer
 ./run.sh
 
 cd ../offline-punctuation
 ./run.sh
+rm -rf sherpa-onnx-*
 
 cd ../speaker-identification
 ./run.sh
 
 cd ../streaming-hlg-decoding/
 ./run.sh
+rm -rf sherpa-onnx-*
 
 cd ../spoken-language-identification
 ./run.sh
+rm -rf sherpa-onnx-*
 
 cd ../offline-tts
 ./run-aishell3.sh
