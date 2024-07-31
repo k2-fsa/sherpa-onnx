daf0df375d77d9d20107839da4c79146a3277b67
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-31 12:31:16 +0800

    remove extra files

--- dart-api-examples/speaker-identification/test/speaker_identification_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:speaker_identification/speaker_identification.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
