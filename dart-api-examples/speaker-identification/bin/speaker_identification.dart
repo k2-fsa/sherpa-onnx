b65f45c162bffceb386c948f0d9206a4fa8d2259
Fangjun Kuang <csukuangfj@gmail.com>
2024-07-31 12:30:58 +0800

    commit after creating the project

---
+++ dart-api-examples/speaker-identification/bin/speaker_identification.dart
@@ -0,0 +1,5 @@
+import 'package:speaker_identification/speaker_identification.dart' as speaker_identification;
+
+void main(List<String> arguments) {
+  print('Hello world: ${speaker_identification.calculate()}!');
+}
---
+++ dart-api-examples/speaker-identification/test/speaker_identification_test.dart
@@ -0,0 +1,8 @@
+import 'package:speaker_identification/speaker_identification.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
