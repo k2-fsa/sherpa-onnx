24ebcea590c816a878ab7d4a6e625efd1dec9698
Fangjun Kuang <csukuangfj@gmail.com>
2024-08-12 07:45:21 +0000

    minor fixes

--- pascal-api-examples/streaming-asr/paraformer.pas
+++ pascal-api-examples/streaming-asr/paraformer.pas
@@ -1,19 +1,19 @@
 { Copyright (c)  2024  Xiaomi Corporation }
 
 {
-This file shows how to use a streaming Zipformer transducer
-to decode files.
+This file shows how to use a streaming Paaraformer model to decode files.
 
 You can download the model files from
 https://github.com/k2-fsa/sherpa-onnx/releases/tag/asr-models
 }
 
-program zipformer_transducer;
+program paraformer;
 
-{$mode objfpc}
+{$mode delphi}
 
 uses
   sherpa_onnx,
+  DateUtils,
   SysUtils;
 
 var
@@ -24,6 +24,13 @@ var
   Wave: TSherpaOnnxWave;
   WaveFilename: AnsiString;
   TailPaddings: array of Single;
+
+  Start: TDateTime;
+  Stop: TDateTime;
+
+  Elapsed: Single;
+  Duration: Single;
+  RealTimeFactor: Single;
 begin
   Initialize(Config);
 
@@ -31,16 +38,22 @@ begin
   to download model files used in this file.}
   Config.ModelConfig.Paraformer.Encoder := './sherpa-onnx-streaming-paraformer-bilingual-zh-en/encoder.int8.onnx';
   Config.ModelConfig.Paraformer.Decoder := './sherpa-onnx-streaming-paraformer-bilingual-zh-en/decoder.int8.onnx';
-  Config.ModelConfig.Tokens := './sherpa-onnx-streaming-paraformer-bilingual-zh-en/tokens.txt"';
+  Config.ModelConfig.Tokens := './sherpa-onnx-streaming-paraformer-bilingual-zh-en/tokens.txt';
+
+  Config.ModelConfig.Provider := 'cpu';
+  Config.ModelConfig.NumThreads := 1;
   Config.ModelConfig.Debug := False;
 
   WaveFilename := './sherpa-onnx-streaming-paraformer-bilingual-zh-en/test_wavs/2.wav';
 
+  Wave := SherpaOnnxReadWave(WaveFilename);
+
   Recognizer := TSherpaOnnxOnlineRecognizer.Create(Config);
+
+  Start := Now;
 
   Stream := Recognizer.CreateStream();
 
-  Wave := SherpaOnnxReadWave(WaveFilename);
 
   Stream.AcceptWaveform(Wave.Samples, Wave.SampleRate);
 
@@ -53,8 +66,18 @@ begin
     Recognizer.Decode(Stream);
 
   RecognitionResult := Recognizer.GetResult(Stream);
-  WriteLn(RecognitionResult.ToString);
 
+  Stop := Now;
+
+  Elapsed := MilliSecondsBetween(Stop, Start) / 1000;
+  Duration := Length(Wave.Samples) / Wave.SampleRate;
+  RealTimeFactor := Elapsed / Duration;
+
+  WriteLn(RecognitionResult.ToString);
+  WriteLn(Format('NumThreads %d', [Config.ModelConfig.NumThreads]));
+  WriteLn(Format('Elapsed %.3f s', [Elapsed]));
+  WriteLn(Format('Wave duration %.3f s', [Duration]));
+  WriteLn(Format('RTF = %.3f/%.3f = %.3f', [Elapsed, Duration, RealTimeFactor]));
 
   {Free resources to avoid memory leak.
 
