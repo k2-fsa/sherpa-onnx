545a258c6c7daa486751ba8498a0d646226f1c1c
Fangjun Kuang <csukuangfj@gmail.com>
2024-08-13 17:10:09 +0800

    Add controls to the dialog

--- lazarus-examples/generate_subtitles/unit1.pas
+++ lazarus-examples/generate_subtitles/unit1.pas
@@ -5,10 +5,20 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    StartBtn: TButton;
+    SelectFileDlg: TOpenDialog;
+    SelectFileBtn: TButton;
+    FileNameEdt: TEdit;
+    procedure FileNameEdtChange(Sender: TObject);
+    procedure SelectFileBtnClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -21,6 +31,30 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  StartBtn.Enabled := False;
+  SelectFileDlg.Filter := 'All Files|*.wav';
+end;
+
+procedure TForm1.SelectFileBtnClick(Sender: TObject);
+begin
+  if SelectFileDlg.Execute then
+    begin
+      FileNameEdt.Text := SelectFileDlg.FileName;
+    end;
+end;
+
+procedure TForm1.FileNameEdtChange(Sender: TObject);
+begin
+  if FileExists(FileNameEdt.Text) then
+    StartBtn.Enabled := True
+  else
+    StartBtn.Enabled := False;
+end;
 
 end.
 
