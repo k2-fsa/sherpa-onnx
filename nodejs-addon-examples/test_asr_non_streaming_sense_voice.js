cddac52780d587444d70b72d7ae4d2d7dfdf1312
Fangjun Kuang <csukuangfj@gmail.com>
2024-09-18 11:03:42 +0800

    Support passing utf-8 strings from JavaScript to C++. (#1355)
    We first convert utf-16 strings to Uint8Array and then we
    pass the array to C++.

--- nodejs-addon-examples/test_asr_non_streaming_sense_voice.js
+++ nodejs-addon-examples/test_asr_non_streaming_sense_voice.js
@@ -3,6 +3,19 @@ const sherpa_onnx = require('sherpa-onnx-node');
 
 // Please download test files from
 // https://github.com/k2-fsa/sherpa-onnx/releases/tag/asr-models
+
+
+// If your path contains non-ascii characters, e.g., Chinese, you can use
+// the following code
+//
+
+// let encoder = new TextEncoder();
+// let tokens = encoder.encode(
+//     './sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/测试.txt');
+// let model = encoder.encode(
+//     './sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/测试.int8.onnx');
+
+
 const config = {
   'featConfig': {
     'sampleRate': 16000,
@@ -12,9 +25,11 @@ const config = {
     'senseVoice': {
       'model':
           './sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/model.int8.onnx',
+      // 'model': model,
       'useInverseTextNormalization': 1,
     },
     'tokens': './sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/tokens.txt',
+    // 'tokens': tokens,
     'numThreads': 2,
     'provider': 'cpu',
     'debug': 1,
