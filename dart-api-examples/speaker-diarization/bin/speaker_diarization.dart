9f65e974adc2cce4c2f23f2c192c9df42df33f73
Fangjun Kuang <csukuangfj@gmail.com>
2024-10-11 18:26:04 +0800

    begin to add dart api examples

---
+++ dart-api-examples/speaker-diarization/bin/speaker_diarization.dart
@@ -0,0 +1,5 @@
+import 'package:speaker_diarization/speaker_diarization.dart' as speaker_diarization;
+
+void main(List<String> arguments) {
+  print('Hello world: ${speaker_diarization.calculate()}!');
+}
---
+++ dart-api-examples/speaker-diarization/test/speaker_diarization_test.dart
@@ -0,0 +1,8 @@
+import 'package:speaker_diarization/speaker_diarization.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
