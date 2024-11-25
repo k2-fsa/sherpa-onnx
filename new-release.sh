4d4d6a9bd550d57874ad13967d7ffaf84b875535
Fangjun Kuang <csukuangfj@gmail.com>
2024-11-25 22:30:19 +0800

    Release v1.10.32

--- new-release.sh
+++ new-release.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sed -i.bak 's/1\.10\.30/1\.10\.31/g' ./build-ios-shared.sh
-find flutter -name *.yaml -type f -exec sed -i.bak 's/1\.10\.30/1\.10\.31/g' {} \;
-find dart-api-examples -name *.yaml -type f -exec sed -i.bak 's/1\.10\.30/1\.10\.31/g' {} \;
-find flutter-examples -name *.yaml -type f -exec sed -i.bak 's/1\.10\.30/1\.10\.31/g' {} \;
-find flutter -name *.podspec -type f -exec sed -i.bak 's/1\.10\.30/1\.10\.31/g' {} \;
-find nodejs-addon-examples -name package.json -type f -exec sed -i.bak 's/1\.10\.30/1\.10\.31/g' {} \;
+sed -i.bak 's/1\.10\.31/1\.10\.32/g' ./build-ios-shared.sh
+find flutter -name *.yaml -type f -exec sed -i.bak 's/1\.10\.31/1\.10\.32/g' {} \;
+find dart-api-examples -name *.yaml -type f -exec sed -i.bak 's/1\.10\.31/1\.10\.32/g' {} \;
+find flutter-examples -name *.yaml -type f -exec sed -i.bak 's/1\.10\.31/1\.10\.32/g' {} \;
+find flutter -name *.podspec -type f -exec sed -i.bak 's/1\.10\.31/1\.10\.32/g' {} \;
+find nodejs-addon-examples -name package.json -type f -exec sed -i.bak 's/1\.10\.31/1\.10\.32/g' {} \;
