0453c810349cb5d0b9ec3e7393d761e213ff4a10
Fangjun Kuang <csukuangfj@gmail.com>
2024-12-12 19:28:28 +0800

    Release v1.10.35

--- new-release.sh
+++ new-release.sh
@@ -2,16 +2,16 @@
 
 set -ex
 
-sed -i.bak 's/1\.10\.33/1\.10\.34/g' ./build-ios-shared.sh
-sed -i.bak 's/1\.10\.33/1\.10\.34/g' ./pom.xml
-sed -i.bak 's/1\.10\.33/1\.10\.34/g' ./jitpack.yml
-sed -i.bak 's/1\.10\.33/1\.10\.34/g' ./android/SherpaOnnxAar/README.md
+sed -i.bak 's/1\.10\.34/1\.10\.35/g' ./build-ios-shared.sh
+sed -i.bak 's/1\.10\.34/1\.10\.35/g' ./pom.xml
+sed -i.bak 's/1\.10\.34/1\.10\.35/g' ./jitpack.yml
+sed -i.bak 's/1\.10\.34/1\.10\.35/g' ./android/SherpaOnnxAar/README.md
 
-find flutter -name *.yaml -type f -exec sed -i.bak 's/1\.10\.33/1\.10\.34/g' {} \;
-find dart-api-examples -name *.yaml -type f -exec sed -i.bak 's/1\.10\.33/1\.10\.34/g' {} \;
-find flutter-examples -name *.yaml -type f -exec sed -i.bak 's/1\.10\.33/1\.10\.34/g' {} \;
-find flutter -name *.podspec -type f -exec sed -i.bak 's/1\.10\.33/1\.10\.34/g' {} \;
-find nodejs-addon-examples -name package.json -type f -exec sed -i.bak 's/1\.10\.33/1\.10\.34/g' {} \;
+find flutter -name *.yaml -type f -exec sed -i.bak 's/1\.10\.34/1\.10\.35/g' {} \;
+find dart-api-examples -name *.yaml -type f -exec sed -i.bak 's/1\.10\.34/1\.10\.35/g' {} \;
+find flutter-examples -name *.yaml -type f -exec sed -i.bak 's/1\.10\.34/1\.10\.35/g' {} \;
+find flutter -name *.podspec -type f -exec sed -i.bak 's/1\.10\.34/1\.10\.35/g' {} \;
+find nodejs-addon-examples -name package.json -type f -exec sed -i.bak 's/1\.10\.34/1\.10\.35/g' {} \;
 
-find harmony-os -name "README.md" -type f -exec sed -i.bak 's/1\.10\.33/1\.10\.34/g' {} \;
-find harmony-os -name oh-package.json5 -type f -exec sed -i.bak 's/1\.10\.33/1\.10\.34/g' {} \;
+find harmony-os -name "README.md" -type f -exec sed -i.bak 's/1\.10\.34/1\.10\.35/g' {} \;
+find harmony-os -name oh-package.json5 -type f -exec sed -i.bak 's/1\.10\.34/1\.10\.35/g' {} \;
