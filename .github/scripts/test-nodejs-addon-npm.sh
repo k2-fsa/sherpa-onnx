33a8534f08aba521bcf46f8fcb5e358934702fee
Fangjun Kuang <csukuangfj@gmail.com>
2025-01-03 14:56:36 +0800

    Add JavaScript API (node-addon-api) for MatchaTTS models.

--- .github/scripts/test-nodejs-addon-npm.sh
+++ .github/scripts/test-nodejs-addon-npm.sh
@@ -85,6 +85,25 @@ fi
 
 echo "----------tts----------"
 
+wget https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/matcha-icefall-en_US-ljspeech.tar.bz2
+tar xvf matcha-icefall-en_US-ljspeech.tar.bz2
+rm matcha-icefall-en_US-ljspeech.tar.bz2
+wget https://github.com/k2-fsa/sherpa-onnx/releases/download/vocoder-models/hifigan_v2.onnx
+
+node ./test_tts_non_streaming_matcha_icefall_en.js
+rm hifigan_v2.onnx
+rm -rf matcha-icefall-en_US-ljspeech
+
+wget https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/matcha-icefall-zh-baker.tar.bz2
+tar xvf matcha-icefall-zh-baker.tar.bz2
+rm matcha-icefall-zh-baker.tar.bz2
+wget https://github.com/k2-fsa/sherpa-onnx/releases/download/vocoder-models/hifigan_v2.onnx
+
+node ./test_tts_non_streaming_matcha_icefall_zh.js
+rm hifigan_v2.onnx
+rm -rf matcha-icefall-zh-baker
+ls -lh *.wav
+
 curl -SL -O https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/vits-piper-en_GB-cori-medium.tar.bz2
 tar xf vits-piper-en_GB-cori-medium.tar.bz2
 rm vits-piper-en_GB-cori-medium.tar.bz2
