85839d29b4a6a4f8ac26141414d7d617c093fd60
Fangjun Kuang <csukuangfj@gmail.com>
2025-01-16 12:37:18 +0800

    Add Python API examples

--- sherpa-onnx/python/csrc/offline-tts-kokoro-model-config.cc
+++ sherpa-onnx/python/csrc/offline-tts-kokoro-model-config.cc
@@ -18,7 +18,7 @@ void PybindOfflineTtsKokoroModelConfig(py::module *m) {
       .def(py::init<const std::string &, const std::string &,
                     const std::string &, const std::string &, float>(),
            py::arg("model"), py::arg("voices"), py::arg("tokens"),
-           py::arg("data_dir") = "", py::arg("length_scale"))
+           py::arg("data_dir"), py::arg("length_scale") = 1.0)
       .def_readwrite("model", &PyClass::model)
       .def_readwrite("voices", &PyClass::voices)
       .def_readwrite("tokens", &PyClass::tokens)
