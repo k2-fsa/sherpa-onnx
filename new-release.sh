66e02d83815c4f4010e81c479bf690e44b865cae
Fangjun Kuang <csukuangfj@gmail.com>
2025-01-22 11:21:31 +0800

    Release v1.10.41 (#1744)

--- new-release.sh
+++ new-release.sh
@@ -2,18 +2,20 @@
 
 set -ex
 
-sed -i.bak 's/1\.10\.39/1\.10\.40/g' ./build-ios-shared.sh
-sed -i.bak 's/1\.10\.39/1\.10\.40/g' ./pom.xml
-sed -i.bak 's/1\.10\.39/1\.10\.40/g' ./jitpack.yml
-sed -i.bak 's/1\.10\.39/1\.10\.40/g' ./android/SherpaOnnxAar/README.md
+sed -i.bak 's/1\.10\.40/1\.10\.41/g' ./build-ios-shared.sh
+sed -i.bak 's/1\.10\.40/1\.10\.41/g' ./pom.xml
+sed -i.bak 's/1\.10\.40/1\.10\.41/g' ./jitpack.yml
+sed -i.bak 's/1\.10\.40/1\.10\.41/g' ./android/SherpaOnnxAar/README.md
 
-find android -name build.gradle -type f -exec sed -i.bak 's/sherpa-onnx:v1\.10\.39/sherpa-onnx:v1\.10\.40/g' {} \;
+find android -name build.gradle -type f -exec sed -i.bak 's/sherpa-onnx:v1\.10\.40/sherpa-onnx:v1\.10\.41/g' {} \;
 
-find flutter -name *.yaml -type f -exec sed -i.bak 's/1\.10\.39/1\.10\.40/g' {} \;
-find dart-api-examples -name *.yaml -type f -exec sed -i.bak 's/1\.10\.39/1\.10\.40/g' {} \;
-find flutter-examples -name *.yaml -type f -exec sed -i.bak 's/1\.10\.39/1\.10\.40/g' {} \;
-find flutter -name *.podspec -type f -exec sed -i.bak 's/1\.10\.39/1\.10\.40/g' {} \;
-find nodejs-addon-examples -name package.json -type f -exec sed -i.bak 's/1\.10\.39/1\.10\.40/g' {} \;
+find flutter -name *.yaml -type f -exec sed -i.bak 's/1\.10\.40/1\.10\.41/g' {} \;
+find dart-api-examples -name *.yaml -type f -exec sed -i.bak 's/1\.10\.40/1\.10\.41/g' {} \;
+find flutter-examples -name *.yaml -type f -exec sed -i.bak 's/1\.10\.40/1\.10\.41/g' {} \;
+find flutter -name *.podspec -type f -exec sed -i.bak 's/1\.10\.40/1\.10\.41/g' {} \;
+find nodejs-addon-examples -name package.json -type f -exec sed -i.bak 's/1\.10\.40/1\.10\.41/g' {} \;
 
-find harmony-os -name "README.md" -type f -exec sed -i.bak 's/1\.10\.39/1\.10\.40/g' {} \;
-find harmony-os -name oh-package.json5 -type f -exec sed -i.bak 's/1\.10\.39/1\.10\.40/g' {} \;
+find harmony-os -name "README.md" -type f -exec sed -i.bak 's/1\.10\.40/1\.10\.41/g' {} \;
+find harmony-os -name oh-package.json5 -type f -exec sed -i.bak 's/1\.10\.40/1\.10\.41/g' {} \;
+
+find . -name "*.bak" -exec rm {} \;
