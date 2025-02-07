284a2215408a5dbd44c7b45a3c460eb104840df4
Fangjun Kuang <csukuangfj@gmail.com>
2025-02-07 15:24:51 +0800

    Add C# API for Kokoro TTS 1.0

--- dotnet-examples/kokoro-tts/run-kokoro.sh
+++ dotnet-examples/kokoro-tts/run-kokoro.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
+if [ ! -f ./kokoro-multi-lang-v1_0/model.onnx ]; then
+  curl -SL -O https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/kokoro-multi-lang-v1_0.tar.bz2
+  tar xf kokoro-multi-lang-v1_0.tar.bz2
+  rm kokoro-multi-lang-v1_0.tar.bz2
+fi
+
 if [ ! -f ./kokoro-en-v0_19/model.onnx ]; then
   curl -SL -O https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/kokoro-en-v0_19.tar.bz2
   tar xf kokoro-en-v0_19.tar.bz2
