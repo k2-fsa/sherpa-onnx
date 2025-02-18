774cf662a4a088c23f555eae7e66e8540432f50b
Fangjun Kuang <csukuangfj@gmail.com>
2025-02-18 12:51:26 +0800

    Fix generating Chinese lexicon for Kokoro TTS 1.0 (#1888)

--- scripts/kokoro/v1.0/generate_lexicon.py
+++ scripts/kokoro/v1.0/generate_lexicon.py
@@ -2,9 +2,20 @@
 # Copyright    2025  Xiaomi Corp.        (authors: Fangjun Kuang)
 
 import json
-from pypinyin import phrases_dict, pinyin_dict
-from misaki import zh
 from typing import List, Tuple
+
+from misaki import zh
+from pypinyin import load_phrases_dict, phrases_dict, pinyin_dict
+
+user_dict = {
+    "还田": [["huan2"], ["tian2"]],
+    "行长": [["hang2"], ["zhang3"]],
+    "银行行长": [["yin2"], ["hang2"], ["hang2"], ["zhang3"]],
+}
+
+load_phrases_dict(user_dict)
+
+phrases_dict.phrases_dict.update(**user_dict)
 
 
 def generate_english_lexicon(kind: str):
@@ -59,11 +70,13 @@ def generate_chinese_lexicon():
         if not (0x4E00 <= key <= 0x9FFF):
             continue
         w = chr(key)
-        tokens: str = g2p(w)
+        tokens: str = g2p.word2ipa(w)
+        tokens = tokens.replace(chr(815), "")
         lexicon.append((w, tokens))
 
     for key in phrases:
-        tokens: str = g2p(key)
+        tokens: str = g2p.word2ipa(key)
+        tokens = tokens.replace(chr(815), "")
         lexicon.append((key, tokens))
     return lexicon
 
