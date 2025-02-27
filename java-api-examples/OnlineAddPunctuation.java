ea7e3121dd2e0b989a7c8f8ac5d5d07a5aebafc8
Fangjun Kuang <csukuangfj@gmail.com>
2025-02-27 16:41:11 +0800

    Add Kotlin and Java API for online punctuation models

--- java-api-examples/OnlineAddPunctuation.java
+++ java-api-examples/OnlineAddPunctuation.java
@@ -1,30 +1,31 @@
-// Copyright 2024 Xiaomi Corporation
+// Copyright 2025 Xiaomi Corporation
 
 // This file shows how to use a punctuation model to add punctuations to text.
 //
 // The model supports both English and Chinese.
 import com.k2fsa.sherpa.onnx.*;
 
-public class AddPunctuation {
+public class OnlineAddPunctuation {
   public static void main(String[] args) {
     // please download the model from
-    // https://github.com/k2-fsa/sherpa-onnx/releases/tag/punctuation-models
-    String model = "./sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12/model.onnx";
-    OfflinePunctuationModelConfig modelConfig =
-        OfflinePunctuationModelConfig.builder()
-            .setCtTransformer(model)
+    // https://github.com/k2-fsa/sherpa-onnx/releases/download/punctuation-models/sherpa-onnx-online-punct-en-2024-08-06.tar.bz2
+    String model = "./sherpa-onnx-online-punct-en-2024-08-06/model.int8.onnx";
+    String bpeVocab = "./sherpa-onnx-online-punct-en-2024-08-06/bpe.vocab";
+    OnlinePunctuationModelConfig modelConfig =
+        OnlinePunctuationModelConfig.builder()
+            .setCnnBilstm(model)
+            .setBpeVocab(bpeVocab)
             .setNumThreads(1)
             .setDebug(true)
             .build();
-    OfflinePunctuationConfig config =
-        OfflinePunctuationConfig.builder().setModel(modelConfig).build();
+    OnlinePunctuationConfig config =
+        OnlinePunctuationConfig.builder().setModel(modelConfig).build();
 
-    OfflinePunctuation punct = new OfflinePunctuation(config);
+    OnlinePunctuation punct = new OnlinePunctuation(config);
 
     String[] sentences =
         new String[] {
-          "这是一个测试你好吗How are you我很好thank you are you ok谢谢你",
-          "我们都是木头人不会说话不会动",
+          "how are you doing fantastic thank you how about you",
           "The African blogosphere is rapidly expanding bringing more voices online in the form of"
               + " commentaries opinions analyses rants and poetry",
         };
--- java-api-examples/run-online-add-punctuation-zh-en.sh
+++ java-api-examples/run-online-add-punctuation-zh-en.sh
@@ -25,13 +25,13 @@ if [ ! -f ../sherpa-onnx/java-api/build/sherpa-onnx.jar ]; then
   popd
 fi
 
-if [ ! -f ./sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12/model.onnx ]; then
-  curl -SL -O https://github.com/k2-fsa/sherpa-onnx/releases/download/punctuation-models/sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12.tar.bz2
-  tar xvf sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12.tar.bz2
-  rm sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12.tar.bz2
+if [ ! -f ./sherpa-onnx-online-punct-en-2024-08-06/model.int8.onnx ]; then
+  curl -SL -O https://github.com/k2-fsa/sherpa-onnx/releases/download/punctuation-models/sherpa-onnx-online-punct-en-2024-08-06.tar.bz2
+  tar xvf sherpa-onnx-online-punct-en-2024-08-06.tar.bz2
+  rm sherpa-onnx-online-punct-en-2024-08-06.tar.bz2
 fi
 
 java \
   -Djava.library.path=$PWD/../build/lib \
   -cp ../sherpa-onnx/java-api/build/sherpa-onnx.jar \
-  ./AddPunctuation.java
+  ./OnlineAddPunctuation.java
