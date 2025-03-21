e658039f957db86ac7db486c7a6b5267e13306d8
Fangjun Kuang <csukuangfj@gmail.com>
2025-03-21 14:05:57 +0800

    Release v1.11.2 (#2035)

--- build-ios-shared.sh
+++ build-ios-shared.sh
@@ -242,7 +242,7 @@ for d in ios-arm64_x86_64-simulator ios-arm64; do
 	<key>CFBundlePackageType</key>
 	<string>FMWK</string>
 	<key>CFBundleShortVersionString</key>
-	<string>1.11.1</string>
+	<string>1.11.2</string>
 	<key>CFBundleSupportedPlatforms</key>
 	<array>
 		<string>iPhoneOS</string>
