cb92184f70aa9dcfcee8e16f6890c0bc0e83bdcb
Eugene Savin <eugene.savin@gmail.com>
2025-05-14 17:27:33 +0300

    Add dither parameter to kotlin FeatureConfig constructor

--- sherpa-onnx/kotlin-api/FeatureConfig.kt
+++ sherpa-onnx/kotlin-api/FeatureConfig.kt
@@ -3,6 +3,7 @@ package com.k2fsa.sherpa.onnx
 data class FeatureConfig(
     var sampleRate: Int = 16000,
     var featureDim: Int = 80,
+    val dither: Float = 0.0f
 )
 
 fun getFeatureConfig(sampleRate: Int, featureDim: Int): FeatureConfig {
