6250c55ae5bafb1257d31338071f804aeb54b211
Fangjun Kuang <csukuangfj@gmail.com>
2025-05-23 14:24:03 +0800

    begin to add spleeter impl

---
+++ sherpa-onnx/csrc/offline-source-separation-spleeter-impl.h
@@ -0,0 +1,10 @@
+// sherpa-onnx/csrc/offline-source-separation-spleeter-impl.h
+//
+// Copyright (c)  2025  Xiaomi Corporation
+
+#ifndef SHERPA_ONNX_CSRC_OFFLINE_SOURCE_SEPARATION_SPLEETER_IMPL_H_
+#define SHERPA_ONNX_CSRC_OFFLINE_SOURCE_SEPARATION_SPLEETER_IMPL_H_
+
+namespace sherpa_onnx {}
+
+#endif  // SHERPA_ONNX_CSRC_OFFLINE_SOURCE_SEPARATION_SPLEETER_IMPL_H_
