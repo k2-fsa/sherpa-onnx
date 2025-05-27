2107afdbd4207cac1f7ef137c4d28f6cdc36ec51
yegyu <f2janyway@gmail.com>
2025-05-27 15:44:06 +0900

    Add include headers for __ANDROID_API__,__OHOS__ (#2251)

--- sherpa-onnx/csrc/offline-source-separation-impl.cc
+++ sherpa-onnx/csrc/offline-source-separation-impl.cc
@@ -5,6 +5,14 @@
 #include "sherpa-onnx/csrc/offline-source-separation-impl.h"
 
 #include <memory>
+#if __ANDROID_API__ >= 9
+#include "android/asset_manager.h"
+#include "android/asset_manager_jni.h"
+#endif
+
+#if __OHOS__
+#include "rawfile/raw_file_manager.h"
+#endif
 
 #include "sherpa-onnx/csrc/offline-source-separation-spleeter-impl.h"
 
