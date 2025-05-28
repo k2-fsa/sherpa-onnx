8eab633ea8d804436c6ff54348d57227c16520dc
Fangjun Kuang <csukuangfj@gmail.com>
2025-05-28 12:18:27 +0800

    minor fixes

--- sherpa-onnx/csrc/offline-source-separation-impl.cc
+++ sherpa-onnx/csrc/offline-source-separation-impl.cc
@@ -5,14 +5,6 @@
 #include "sherpa-onnx/csrc/offline-source-separation-impl.h"
 
 #include <memory>
-#if __ANDROID_API__ >= 9
-#include "android/asset_manager.h"
-#include "android/asset_manager_jni.h"
-#endif
-
-#if __OHOS__
-#include "rawfile/raw_file_manager.h"
-#endif
 
 #if __ANDROID_API__ >= 9
 #include "android/asset_manager.h"
