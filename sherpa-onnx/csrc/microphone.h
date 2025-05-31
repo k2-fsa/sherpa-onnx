613e8084c2d1df13606e3131554b0a79a1f72172
mtdxc <mtdxc@126.com>
2025-05-31 21:48:41 +0800

    move portaudio common record code to microphone (#2264)
    Co-authored-by: cqm <cqm@97kid.com>

--- sherpa-onnx/csrc/microphone.h
+++ sherpa-onnx/csrc/microphone.h
@@ -4,13 +4,22 @@
 
 #ifndef SHERPA_ONNX_CSRC_MICROPHONE_H_
 #define SHERPA_ONNX_CSRC_MICROPHONE_H_
+#include "portaudio.h"  // NOLINT
 
 namespace sherpa_onnx {
 
 class Microphone {
+  PaStream *stream = nullptr;
  public:
   Microphone();
   ~Microphone();
+
+  int GetDeviceCount() const;
+  int GetDefaultInputDevice() const;
+  void PrintDevices(int sel) const;
+  
+  bool OpenDevice(int index, int sample_rate, int channel, PaStreamCallback cb, void* userdata);
+  void CloseDevice();
 };
 
 }  // namespace sherpa_onnx
--- sherpa-onnx/csrc/sherpa-onnx-microphone-offline.cc
+++ sherpa-onnx/csrc/sherpa-onnx-microphone-offline.cc
@@ -136,11 +136,7 @@ for a list of pre-trained models to download.
 
   sherpa_onnx::Microphone mic;
 
-  PaDeviceIndex num_devices = Pa_GetDeviceCount();
-  fprintf(stderr, "Num devices: %d\n", num_devices);
-
   int32_t device_index = Pa_GetDefaultInputDevice();
-
   if (device_index == paNoDevice) {
     fprintf(stderr, "No default input device found\n");
     fprintf(stderr, "If you are using Linux, please switch to \n");
@@ -154,26 +150,8 @@ for a list of pre-trained models to download.
     device_index = atoi(pDeviceIndex);
   }
 
-  for (int32_t i = 0; i != num_devices; ++i) {
-    const PaDeviceInfo *info = Pa_GetDeviceInfo(i);
-    fprintf(stderr, " %s %d %s\n", (i == device_index) ? "*" : " ", i,
-            info->name);
-  }
+  mic.PrintDevices(device_index);
 
-  PaStreamParameters param;
-  param.device = device_index;
-
-  fprintf(stderr, "Use device: %d\n", param.device);
-
-  const PaDeviceInfo *info = Pa_GetDeviceInfo(param.device);
-  fprintf(stderr, "  Name: %s\n", info->name);
-  fprintf(stderr, "  Max input channels: %d\n", info->maxInputChannels);
-
-  param.channelCount = 1;
-  param.sampleFormat = paFloat32;
-
-  param.suggestedLatency = info->defaultLowInputLatency;
-  param.hostApiSpecificStreamInfo = nullptr;
   float mic_sample_rate = 16000;
   const char *pSampleRateStr = std::getenv("SHERPA_ONNX_MIC_SAMPLE_RATE");
   if (pSampleRateStr) {
@@ -181,24 +159,9 @@ for a list of pre-trained models to download.
     mic_sample_rate = atof(pSampleRateStr);
   }
 
-  PaStream *stream;
-  PaError err =
-      Pa_OpenStream(&stream, &param, nullptr, /* &outputParameters, */
-                    mic_sample_rate,
-                    0,          // frames per buffer
-                    paClipOff,  // we won't output out of range samples
-                                // so don't bother clipping them
-                    RecordCallback, nullptr);
-  if (err != paNoError) {
-    fprintf(stderr, "portaudio error: %s\n", Pa_GetErrorText(err));
-    exit(EXIT_FAILURE);
-  }
-
-  err = Pa_StartStream(stream);
-  fprintf(stderr, "Started\n");
-
-  if (err != paNoError) {
-    fprintf(stderr, "portaudio error: %s\n", Pa_GetErrorText(err));
+  if (!mic.OpenDevice(device_index, mic_sample_rate, 1,
+                 RecordCallback, nullptr /* user_data */)){
+    fprintf(stderr, "portaudio error: %d\n", device_index);
     exit(EXIT_FAILURE);
   }
 
@@ -231,12 +194,6 @@ for a list of pre-trained models to download.
     Pa_Sleep(20);  // sleep for 20ms
   }
   t.join();
-
-  err = Pa_CloseStream(stream);
-  if (err != paNoError) {
-    fprintf(stderr, "portaudio error: %s\n", Pa_GetErrorText(err));
-    exit(EXIT_FAILURE);
-  }
 
   return 0;
 }
