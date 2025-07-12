f2df11672606a7be372259edaf1dbdf3469069f6
Fangjun Kuang <csukuangfj@gmail.com>
2025-07-12 12:36:31 +0800

    Add CXX API examples for ten-vad. (#2380)

--- cxx-api-examples/vad-cxx-api.cc
+++ cxx-api-examples/vad-cxx-api.cc
@@ -6,7 +6,12 @@
 // This file demonstrates how to use VAD to remove silences from a file
 // clang-format off
 //
-// wget https://github.com/k2-fsa/sherpa-onnx/releases/download/asr-models/silero_vad.onnx
+// To use silero-vad:
+//  wget https://github.com/k2-fsa/sherpa-onnx/releases/download/asr-models/silero_vad.onnx
+//
+// To use ten-vad:
+//  wget https://github.com/k2-fsa/sherpa-onnx/releases/download/asr-models/ten-vad.onnx
+//
 // wget https://github.com/k2-fsa/sherpa-onnx/releases/download/asr-models/lei-jun-test.wav
 //
 // clang-format on
@@ -19,14 +24,45 @@ int32_t main() {
   using namespace sherpa_onnx::cxx;  // NOLINT
 
   std::string wave_filename = "./lei-jun-test.wav";
-  std::string vad_filename = "./silero_vad.onnx";
+  if (!FileExists(wave_filename)) {
+    fprintf(stderr, "Please download %s\n", wave_filename.c_str());
+    return -1;
+  }
+
+  std::string vad_filename;
+  bool use_silero_vad = false;
+  bool use_ten_vad = false;
+
+  if (FileExists("./silero_vad.onnx")) {
+    printf("Use silero-vad\n");
+    vad_filename = "./silero_vad.onnx";
+    use_silero_vad = true;
+  } else if (FileExists("./ten-vad.onnx")) {
+    printf("Use ten-vad\n");
+    vad_filename = "./ten-vad.onnx";
+    use_ten_vad = true;
+  } else {
+    fprintf(stderr, "Please provide either silero_vad.onnx or ten-vad.onnx\n");
+    return -1;
+  }
 
   VadModelConfig config;
-  config.silero_vad.model = vad_filename;
-  config.silero_vad.threshold = 0.1;
-  config.silero_vad.min_silence_duration = 0.5;
-  config.silero_vad.min_speech_duration = 0.25;
-  config.silero_vad.max_speech_duration = 20;
+  if (use_silero_vad) {
+    config.silero_vad.model = vad_filename;
+    config.silero_vad.threshold = 0.3;
+    config.silero_vad.min_silence_duration = 0.5;
+    config.silero_vad.min_speech_duration = 0.25;
+    config.silero_vad.max_speech_duration = 20;
+    config.silero_vad.window_size = 512;
+  } else if (use_ten_vad) {
+    config.ten_vad.model = vad_filename;
+    config.ten_vad.threshold = 0.3;
+    config.ten_vad.min_silence_duration = 0.5;
+    config.ten_vad.min_speech_duration = 0.25;
+    config.ten_vad.max_speech_duration = 20;
+    config.ten_vad.window_size = 256;
+  }
+
   config.sample_rate = 16000;
   config.debug = true;
 
@@ -43,7 +79,8 @@ int32_t main() {
   }
   bool is_eof = false;
   int32_t i = 0;
-  int32_t window_size = config.silero_vad.window_size;
+  int32_t window_size = use_silero_vad ? config.silero_vad.window_size
+                                       : config.ten_vad.window_size;
 
   int32_t sample_rate = config.sample_rate;
 
