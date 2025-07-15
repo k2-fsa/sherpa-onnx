a53bfd89094623c45f84175d1649250a455670db
k2-fsa <csukuangfj@gmail.com>
2025-07-15 18:54:25 +0800

    begin to add the SimulateStreamingAsr object

---
+++ android/SherpaOnnxSimulateStreamingAsrWearOs/app/src/main/java/com/k2fsa/sherpa/onnx/simulate/streaming/asr/wear/os/presentation/SimulateStreamingAsr.kt
@@ -0,0 +1,7 @@
+package com.k2fsa.sherpa.onnx.simulate.streaming.asr.wear.os.presentation
+import com.k2fsa.sherpa.onnx.OfflineRecognizer
+
+
+object SimulateStreamingAsr {
+    private var _recognizer: OfflineRecognizer? = null
+}
