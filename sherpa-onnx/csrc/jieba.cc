a37f13421641fd0808d8c7c7b99ee6fb0c3c490f
Fangjun Kuang <csukuangfj@gmail.com>
2025-07-27 21:08:04 +0800

    Add friendly log messages for Android and HarmonyOs TTS users.

--- sherpa-onnx/csrc/jieba.cc
+++ sherpa-onnx/csrc/jieba.cc
@@ -5,6 +5,7 @@
 #include "sherpa-onnx/csrc/jieba.h"
 
 #include "sherpa-onnx/csrc/file-utils.h"
+#include "sherpa-onnx/csrc/macros.h"
 
 namespace sherpa_onnx {
 
@@ -18,6 +19,21 @@ std::unique_ptr<cppjieba::Jieba> InitJieba(const std::string &dict_dir) {
   std::string user_dict = dict_dir + "/user.dict.utf8";
   std::string idf = dict_dir + "/idf.utf8";
   std::string stop_word = dict_dir + "/stop_words.utf8";
+
+#if __ANDROID_API__ >= 9 || defined(__OHOS__)
+  if (dict[0] != '/') {
+    SHERPA_ONNX_LOGE(
+        "You need to follow our examples to copy the jieba dict directory from "
+        "the assets folder to an external storage directory");
+
+    SHERPA_ONNX_LOGE(
+        "Hint: Please see\n"
+        "https://github.com/k2-fsa/sherpa-onnx/blob/master/android/"
+        "SherpaOnnxTtsEngine/app/src/main/java/com/k2fsa/sherpa/onnx/tts/"
+        "engine/TtsEngine.kt#L193\n"
+        "The function copyDataDir()\n");
+  }
+#endif
 
   AssertFileExists(dict);
   AssertFileExists(hmm);
