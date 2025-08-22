a7200e8efbab69a017f2c03a2471fd94e629e422
k2-fsa <csukuangfj@gmail.com>
2025-08-22 20:18:53 +0800

    Support numpy 2.0

--- sherpa-onnx/python/csrc/sherpa-onnx.h
+++ sherpa-onnx/python/csrc/sherpa-onnx.h
@@ -5,6 +5,8 @@
 #ifndef SHERPA_ONNX_PYTHON_CSRC_SHERPA_ONNX_H_
 #define SHERPA_ONNX_PYTHON_CSRC_SHERPA_ONNX_H_
 
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+
 #include "pybind11/functional.h"
 #include "pybind11/numpy.h"
 #include "pybind11/pybind11.h"
