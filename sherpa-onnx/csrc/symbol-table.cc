9d0adcd3f53e4594112d66ebc6bfe213243121f6
Fangjun Kuang <csukuangfj@gmail.com>
2025-08-26 12:03:02 +0800

    Support BPE models with byte fallback. (#2531)

--- sherpa-onnx/csrc/symbol-table.cc
+++ sherpa-onnx/csrc/symbol-table.cc
@@ -171,6 +171,12 @@ SymbolTable::SymbolTable(Manager *mgr, const std::string &filename) {
 void SymbolTable::Init(std::istream &is) {
   sym2id_ = ReadTokens(is, &id2sym_);
   is_bbpe_ = IsByteBPE(sym2id_);
+
+  if (sym2id_.count("<0x00>") && sym2id_.count("<0xFF>") &&
+      ((sym2id_.at("<0xFF>") - sym2id_.at("<0x00>")) == 255)) {
+    is_bpe_with_byte_fallback_ = true;
+    id_for_0x00_ = sym2id_.at("<0x00>");
+  }
 }
 
 std::string SymbolTable::ToString() const {
@@ -197,13 +203,13 @@ const std::string SymbolTable::operator[](int32_t id) const {
   // id 0 is blank, id 1 is sos/eos, id 2 is unk
   //
   // Note: For moonshine models, 0 is <unk>, 1, is <s>, 2 is</s>
-  if (id >= 3 && id <= 258 && sym.size() == 6 && sym[0] == '<' &&
+  if (is_bpe_with_byte_fallback_ && sym.size() == 6 && sym[0] == '<' &&
       sym[1] == '0' && sym[2] == 'x' && sym[5] == '>') {
     std::ostringstream os;
-    os << std::hex << std::uppercase << (id - 3);
+    os << std::hex << std::uppercase << (id - id_for_0x00_);
 
     if (std::string(sym.data() + 3, sym.data() + 5) == os.str()) {
-      uint8_t i = id - 3;
+      uint8_t i = id - id_for_0x00_;
       sym = std::string(&i, &i + 1);
     }
   }
