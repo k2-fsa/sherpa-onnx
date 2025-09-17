925cb8d05c35cd9f0b74046cf000515da3c0a849
colourmebrad <bradmallett@gmail.com>
2025-09-17 09:52:20 -0400

    script was compiling library twice

--- build-node-addon.sh
+++ build-node-addon.sh
@@ -1,20 +1,11 @@
-# PHASE ONE - Build Sherpa-Onnx
+# Build Sherpa-Onnx + Node Addon
 
-rm -rf build-sherpa-onnx
-mkdir build-sherpa-onnx
-cd build-sherpa-onnx
-cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=./install ..
-make install
-export SHERPA_ONNX_INSTALL_DIR=$PWD/install
-
-# PHASE TWO - Build Node Addon
-
-cd ..
-rm -rf build-node-addon
-mkdir build-node-addon
-cd build-node-addon
+rm -rf build
+mkdir build
+cd build
 cmake -DCMAKE_INSTALL_PREFIX=./install -DBUILD_SHARED_LIBS=ON ..
 make -j install
+export SHERPA_ONNX_INSTALL_DIR=$PWD/install #
 export PKG_CONFIG_PATH=$PWD/install:$PKG_CONFIG_PATH
 cd ../scripts/node-addon-api/
 npm i
