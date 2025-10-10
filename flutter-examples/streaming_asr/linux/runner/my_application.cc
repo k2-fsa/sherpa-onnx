8415a6639cec9d173b86e37a2946331cbec9dc30
Fangjun Kuang <csukuangfj@gmail.com>
2025-10-10 19:32:59 +0800

    Fix linux

--- flutter-examples/streaming_asr/linux/runner/my_application.cc
+++ flutter-examples/streaming_asr/linux/runner/my_application.cc
@@ -62,7 +62,9 @@ static void my_application_activate(GApplication* application) {
   GdkRGBA background_color;
   // Background defaults to black, override it here if necessary, e.g. #00000000 for transparent.
   gdk_rgba_parse(&background_color, "#000000");
-  fl_view_set_background_color(view, &background_color);
+  // fl_view_set_background_color(view, &background_color);
+  gtk_widget_override_background_color(GTK_WIDGET(view), GTK_STATE_FLAG_NORMAL, &background_color);
+
   gtk_widget_show(GTK_WIDGET(view));
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
 
