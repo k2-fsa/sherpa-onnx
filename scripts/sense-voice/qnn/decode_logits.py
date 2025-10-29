b34243061998dc9ad012034a31f7630a3a96535e
Fangjun Kuang <csukuangfj@gmail.com>
2025-10-29 18:39:56 +0800

    Export SenseVoice to qnn

---
+++ scripts/sense-voice/qnn/decode_logits.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def load_tokens(filename):
+    ans = dict()
+    i = 0
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            ans[i] = line.strip().split()[0]
+            i += 1
+    return ans
+
+
+logits1 = np.fromfile("./logits.raw", dtype=np.float32).reshape((-1, 25055))
+print(logits.shape)
+
+idx = logits.argmax(axis=-1)
+print("idx", idx)
+print(len(idx))
+prev = -1
+ids = []
+for i in idx:
+    if i != prev:
+        ids.append(i)
+    prev = i
+ids = [i for i in ids if i != 0]
+print(ids)
+
+tokens = load_tokens("./tokens.txt")
+text = "".join([tokens[i] for i in ids])
+
+text = text.replace("_", " ")
+print(text)
