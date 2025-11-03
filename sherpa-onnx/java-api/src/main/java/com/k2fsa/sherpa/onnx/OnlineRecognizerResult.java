29763c78ff6a0ce38e3d41998d49c9ca96e3bd7e
esavin <eugene.savin@gmail.com>
2025-11-03 09:19:57 +0300

    Expose ys probs to JNI, Kotlin and Java API (#2736)

--- sherpa-onnx/java-api/src/main/java/com/k2fsa/sherpa/onnx/OnlineRecognizerResult.java
+++ sherpa-onnx/java-api/src/main/java/com/k2fsa/sherpa/onnx/OnlineRecognizerResult.java
@@ -6,11 +6,13 @@ public class OnlineRecognizerResult {
     private final String text;
     private final String[] tokens;
     private final float[] timestamps;
+    private final float[] ysProbs;
 
-    public OnlineRecognizerResult(String text, String[] tokens, float[] timestamps) {
+    public OnlineRecognizerResult(String text, String[] tokens, float[] timestamps, float[] ysProbs) {
         this.text = text;
         this.tokens = tokens;
         this.timestamps = timestamps;
+        this.ysProbs = ysProbs;
     }
 
     public String getText() {
@@ -24,4 +26,9 @@ public class OnlineRecognizerResult {
     public float[] getTimestamps() {
         return timestamps;
     }
+
+    public float[] getYsProbs() {
+        return ysProbs;
+    }
+
 }
