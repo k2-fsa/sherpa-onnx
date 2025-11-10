7173a268fbd3c3a65c87e6b2e68589e4470ec677
Fangjun Kuang <csukuangfj@gmail.com>
2025-11-10 11:28:36 +0800

    Fixes after review

--- cxx-api-examples/offline-punctuation-cxx-api.cc
+++ cxx-api-examples/offline-punctuation-cxx-api.cc
@@ -3,9 +3,8 @@
 
 // To use punctuation model:
 // clang-format off
-// wget
-// https://github.com/k2-fsa/sherpa-onnx/releases/download/punctuation-models/sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12-int8..tar.bz2
-// tar xvf sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12-int8.tar.bz2 
+// wget https://github.com/k2-fsa/sherpa-onnx/releases/download/punctuation-models/sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12-int8.tar.bz2
+// tar xvf sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12-int8.tar.bz2
 // rm sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12-int8.tar.bz2
 // clang-format on
 
--- cxx-api-examples/online-punctuation-cxx-api.cc
+++ cxx-api-examples/online-punctuation-cxx-api.cc
@@ -6,7 +6,7 @@
 // wget https://github.com/k2-fsa/sherpa-onnx/releases/download/punctuation-models/sherpa-onnx-online-punct-en-2024-08-06.tar.bz2
 // tar xvf sherpa-onnx-online-punct-en-2024-08-06.tar.bz2
 // rm sherpa-onnx-online-punct-en-2024-08-06.tar.bz2
-// clang-format off
+// clang-format on
 
 #include <iostream>
 #include <string>
@@ -17,8 +17,10 @@ int32_t main() {
   using namespace sherpa_onnx::cxx;  // NOLINT
 
   OnlinePunctuationConfig punctuation_config;
-  punctuation_config.model.cnn_bilstm = "sherpa-onnx-online-punct-en-2024-08-06/model.int8.onnx";
-  punctuation_config.model.bpe_vocab = "sherpa-onnx-online-punct-en-2024-08-06/bpe.vocab";
+  punctuation_config.model.cnn_bilstm =
+      "sherpa-onnx-online-punct-en-2024-08-06/model.int8.onnx";
+  punctuation_config.model.bpe_vocab =
+      "sherpa-onnx-online-punct-en-2024-08-06/bpe.vocab";
   punctuation_config.model.num_threads = 1;
   punctuation_config.model.debug = false;
   punctuation_config.model.provider = "cpu";
