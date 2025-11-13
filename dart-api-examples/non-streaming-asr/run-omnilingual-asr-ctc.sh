dd7b3d0f288358747501326bfd0d4c6f2f320419
Fangjun Kuang <csukuangfj@gmail.com>
2025-11-13 18:10:18 +0800

    Minor fixes

--- dart-api-examples/non-streaming-asr/run-omnilingual-asr-ctc.sh
+++ dart-api-examples/non-streaming-asr/run-omnilingual-asr-ctc.sh
@@ -5,7 +5,7 @@ set -ex
 dart pub get
 
 if [ ! -f sherpa-onnx-omnilingual-asr-1600-languages-300M-ctc-int8-2025-11-12/tokens.txt ]; then
-  wget https://github.com/k2-fsa/sherpa-onnx/releases/download/asr-models/sherpa-onnx-omnilingual-asr-1600-languages-300M-ctc-int8-2025-11-12.tar.bz2
+  curl -SL -O https://github.com/k2-fsa/sherpa-onnx/releases/download/asr-models/sherpa-onnx-omnilingual-asr-1600-languages-300M-ctc-int8-2025-11-12.tar.bz2
   tar xvf sherpa-onnx-omnilingual-asr-1600-languages-300M-ctc-int8-2025-11-12.tar.bz2
   rm sherpa-onnx-omnilingual-asr-1600-languages-300M-ctc-int8-2025-11-12.tar.bz2
 fi
