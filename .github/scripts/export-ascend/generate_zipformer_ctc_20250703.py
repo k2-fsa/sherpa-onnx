de20f97b0241a9b578b3cfef60a4b59f1c1ca495
Fangjun Kuang <csukuangfj@gmail.com>
2025-11-25 14:43:25 +0800

    export more models

--- .github/scripts/export-ascend/generate_zipformer_ctc_20250703.py
+++ .github/scripts/export-ascend/generate_zipformer_ctc_20250703.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # Copyright    2025  Xiaomi Corp.        (authors: Fangjun Kuang)
 
-from dataclasses import dataclass, asdict
-
-import json
 import itertools
+import json
+from dataclasses import asdict, dataclass
 
 
 def get_image(cann: str, soc_version: str):
@@ -37,10 +36,6 @@ class Config:
 
     num_seconds: int
 
-    # "quay.io/ascend/cann:7.0.1.beta1-910b-ubuntu22.04-py3.8"
-    # "gpustack/ascendai-cann:8.0.RC3-910b-ubuntu20.04-py3.9"
-    # "gpustack/devel-ascendai-cann:8.2.rc1-910b-ubuntu20.04-v2"
-    # "quay.io/ascend/cann:7.0.1-310p-ubuntu22.04-py3.9"
     image: str = ""
 
     def __post_init__(self):
@@ -51,10 +46,6 @@ def main():
     cann_version = ["7.0", "8.0", "8.2"]
     soc_version = ["910B", "910B2", "910B3", "310P3"]
     input_in_seconds = ["5", "8", "10", "13", "15", "18", "20", "23", "25", "28", "30"]
-
-    cann_version = ["7.0", "8.0"]
-    soc_version = ["910B", "910B2"]
-    input_in_seconds = ["5", "8"]
 
     configs = []
     for cann, soc, sec in itertools.product(
