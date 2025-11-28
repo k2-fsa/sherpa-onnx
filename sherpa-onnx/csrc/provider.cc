bb4a7475aa41a2dc8afc413d187570a20ef33b28
Alex <jinghui.huang@spacemit.com>
2025-11-28 17:26:26 +0800

    add spacemit ort ep

--- sherpa-onnx/csrc/provider.cc
+++ sherpa-onnx/csrc/provider.cc
@@ -29,6 +29,8 @@ Provider StringToProvider(std::string s) {
     return Provider::kTRT;
   } else if (s == "directml") {
     return Provider::kDirectML;
+  } else if (s == "spacemit") {
+    return Provider::kSpacemiT;
   } else {
     SHERPA_ONNX_LOGE("Unsupported string: %s. Fallback to cpu", s.c_str());
     return Provider::kCPU;
