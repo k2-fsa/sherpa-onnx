212f5f1c743b4794df855cb557eabccbc38e05a0
ming030890 <67713085+ming030890@users.noreply.github.com>
2025-12-04 12:29:24 +0000

    Adjust negative threshold logic in silero-vad-model
    Also clamp it to 0.01

--- sherpa-onnx/csrc/silero-vad-model.cc
+++ sherpa-onnx/csrc/silero-vad-model.cc
@@ -131,7 +131,13 @@ class SileroVadModel::Impl {
       return false;
     }
 
-    if ((prob > threshold - 0.15) && triggered_) {
+    float neg_threshold;
+    if (config_.silero_vad.neg_threshold < 0) {
+        neg_threshold = std::max(threshold - 0.15f, 0.01f);
+    } else {
+        neg_threshold = std::max(config_.silero_vad.neg_threshold, 0.01f);
+    }
+    if ((prob > neg_threshold) && triggered_) {
       // speaking
       return true;
     }
