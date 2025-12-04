56e1aafb585c2c3931ea8b07dcd52e8300c687e9
Fangjun Kuang <csukuangfj@gmail.com>
2025-12-04 19:27:16 +0800

    Fix a typo

--- scripts/matcha-tts/zh-en/generate_samples.py
+++ scripts/matcha-tts/zh-en/generate_samples.py
@@ -28,7 +28,7 @@ if not config.validate():
     raise ValueError("Please check your config")
 
 tts = sherpa_onnx.OfflineTts(config)
-text = "我最近在学习machine learning，希望能够在未来的artificial intelligence领域有所建树。在这次vacation中，我们计划去Paris欣赏埃菲尔铁塔和卢浮宫的美景。某某银行的副行长和一些行政领导表示，他们去过长江和长白山; 经济不断增长。开始数字测试。2025年12月4号，拨打110或者189202512043。123456块钱。在这个快速发展的时代，人工智能技术正在改变我们的生活方式。语音合成作为人工智能的重要应用之一，让机器能够用自然流畅的语音与人类进行交流。"
+text = "我最近在学习machine learning，希望能够在未来的artificial intelligence领域有所建树。在这次vocation中，我们计划去Paris欣赏埃菲尔铁塔和卢浮宫的美景。某某银行的副行长和一些行政领导表示，他们去过长江和长白山; 经济不断增长。开始数字测试。2025年12月4号，拨打110或者189202512043。123456块钱。在这个快速发展的时代，人工智能技术正在改变我们的生活方式。语音合成作为人工智能的重要应用之一，让机器能够用自然流畅的语音与人类进行交流。"
 
 
 audio = tts.generate(text, sid=0, speed=1.0)
