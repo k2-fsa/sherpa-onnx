a829b12759b4172a041e0b742b926b517819d85d
Fangjun Kuang <csukuangfj@gmail.com>
2025-12-04 11:17:42 +0800

    add some comments

--- sherpa-onnx/csrc/matcha-tts-lexicon.cc
+++ sherpa-onnx/csrc/matcha-tts-lexicon.cc
@@ -39,6 +39,9 @@ namespace sherpa_onnx {
 
 namespace {
 // code in this anonymous namespace is written by ChatGPT
+//
+// Please see https://github.com/k2-fsa/sherpa-onnx/pull/2853
+// for why we need to do the replacement
 static const std::vector<std::pair<std::string, std::string>> kReplacements = {
     {"ɝ", "ɜɹ"}, {"ɚ", "əɹ"},
 
