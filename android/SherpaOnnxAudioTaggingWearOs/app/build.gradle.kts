2202e2a59ba7687dd007afcd0b3050a3af7e5d6c
Fangjun Kuang <csukuangfj@mails.tsinghua.edu.cn>
2025-12-05 20:44:58 +0800

    Release v1.12.19 (#2868)

--- android/SherpaOnnxAudioTaggingWearOs/app/build.gradle.kts
+++ android/SherpaOnnxAudioTaggingWearOs/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.k2fsa.sherpa.onnx.audio.tagging.wear.os"
         minSdk = 26
         targetSdk = 34
-        versionCode = 20251127
-        versionName = "1.12.18"
+        versionCode = 20251205
+        versionName = "1.12.19"
         vectorDrawables {
             useSupportLibrary = true
         }
