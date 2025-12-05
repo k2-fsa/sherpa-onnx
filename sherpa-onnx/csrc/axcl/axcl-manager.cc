f21a8bcf46dda2d02050252a4f539606087c8a16
Fangjun Kuang <csukuangfj@gmail.com>
2025-12-05 15:28:07 +0800

    Add AxclManager

---
+++ sherpa-onnx/csrc/axcl/axcl-manager.cc
@@ -0,0 +1,42 @@
+// sherpa-onnx/csrc/axcl/axcl-manager.cc
+//
+// Copyright (c)  2025  Xiaomi Corporation
+
+#include "sherpa-onnx/csrc/axcl/axcl-manager.h"
+
+#include "axcl.h"  // NOLINT
+#include "sherpa-onnx/csrc/macros.h"
+
+namespace sherpa_onnx {
+
+std::mutex AxclManager::mutex_;
+
+std::atomic<int> AxclManager::instanceCount_{0};
+
+AxclManager::AxclManager(const char *config /*= nullptr*/) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  if (instanceCount_++ == 0) {
+    auto ret = axclInit(config);
+    if (!ret) {
+      instanceCount_--;
+      SHERPA_ONNX_LOGE("Failed to call axclInit(). Return code: %d",
+                       static_cast<int32_t>(ret));
+      SHERPA_ONNX_EXIT(-1);
+    }
+  }
+}
+
+AxclManager::~AxclManager() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  if (--instanceCount_ == 0) {
+    auto ret = axclFinalize();
+
+    if (!ret) {
+      SHERPA_ONNX_LOGE("Failed to call axclFinalize(). Return code: %d",
+                       static_cast<int32_t>(ret));
+      SHERPA_ONNX_EXIT(-1);
+    }
+  }
+}
+
+}  // namespace sherpa_onnx
