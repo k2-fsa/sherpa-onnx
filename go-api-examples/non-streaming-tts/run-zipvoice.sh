560bf214070283168d12ac548a1dd5293997c1ff
Fangjun Kuang <csukuangfj@gmail.com>
2025-12-11 20:27:35 +0800

    Fixes after review

--- go-api-examples/non-streaming-tts/run-zipvoice.sh
+++ go-api-examples/non-streaming-tts/run-zipvoice.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # to download more models
-if [ ! -f ./sherpa-onnx-zipvoice-distill-int8-zh-en-emilia/fm_decoder.onnx ]; then
+if [ ! -f ./sherpa-onnx-zipvoice-distill-int8-zh-en-emilia/encoder.int8.onnx ]; then
   curl -SL -O https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/sherpa-onnx-zipvoice-distill-int8-zh-en-emilia.tar.bz2
   tar xf sherpa-onnx-zipvoice-distill-int8-zh-en-emilia.tar.bz2
   rm sherpa-onnx-zipvoice-distill-int8-zh-en-emilia.tar.bz2
