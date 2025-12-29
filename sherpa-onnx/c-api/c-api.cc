13b8b84a892fee9e0dabc56e696c7c9a79cea728
Fangjun Kuang <csukuangfj@gmail.com>
2025-12-29 09:56:08 +0800

    Add C and CXX API for Google MedASR model (#2946)

--- sherpa-onnx/c-api/c-api.cc
+++ sherpa-onnx/c-api/c-api.cc
@@ -511,6 +511,9 @@ static sherpa_onnx::OfflineRecognizerConfig GetOfflineRecognizerConfig(
   recognizer_config.model_config.omnilingual.model =
       SHERPA_ONNX_OR(config->model_config.omnilingual.model, "");
 
+  recognizer_config.model_config.medasr.model =
+      SHERPA_ONNX_OR(config->model_config.medasr.model, "");
+
   recognizer_config.lm_config.model =
       SHERPA_ONNX_OR(config->lm_config.model, "");
   recognizer_config.lm_config.scale =
