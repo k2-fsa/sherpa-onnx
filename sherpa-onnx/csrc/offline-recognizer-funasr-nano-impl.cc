054a68a93c449d8f7f432cefc54817d7b45949e2
Fangjun Kuang <csukuangfj@gmail.com>
2026-01-04 17:28:13 +0800

    Fix building for HarmonyOS

--- sherpa-onnx/csrc/offline-recognizer-funasr-nano-impl.cc
+++ sherpa-onnx/csrc/offline-recognizer-funasr-nano-impl.cc
@@ -14,6 +14,15 @@
 #include <utility>
 #include <vector>
 
+#if __ANDROID_API__ >= 9
+#include "android/asset_manager.h"
+#include "android/asset_manager_jni.h"
+#endif
+
+#if __OHOS__
+#include "rawfile/raw_file_manager.h"
+#endif
+
 #include "onnxruntime_cxx_api.h"
 #include "sherpa-onnx/csrc/macros.h"
 #include "sherpa-onnx/csrc/onnx-utils.h"
