0126e89659ee8e5a5797de2fb9d9be7223622f7d
Fangjun Kuang <csukuangfj@gmail.com>
2026-01-12 18:43:01 +0800

    small fixes

--- nodejs-examples/test-offline-funasr-nano.js
+++ nodejs-examples/test-offline-funasr-nano.js
@@ -1,24 +1,5 @@
 // Copyright (c)  2026  Xiaomi Corporation (authors: Fangjun Kuang)
 //
-/*
-This script throws the following error since the model is too large
-for WebAssembly to run it.
-
-wasm://wasm/0308b752:1
-RuntimeError: memory access out of bounds
-    at wasm://wasm/0308b752:wasm-function[66]:0xe3ca
-    at wasm://wasm/0308b752:wasm-function[134]:0x1464f
-    at wasm://wasm/0308b752:wasm-function[4492]:0x34e46b
-    at wasm://wasm/0308b752:wasm-function[9931]:0x6c8fc1
-    at new OfflineRecognizer (/home/runner/work/sherpa-onnx/sherpa-onnx/scripts/nodejs/sherpa-onnx-asr.js:1530:27)
-    at Object.createOfflineRecognizer (/home/runner/work/sherpa-onnx/sherpa-onnx/scripts/nodejs/index.js:22:10)
-    at createOfflineRecognizer (/home/runner/work/sherpa-onnx/sherpa-onnx/scripts/nodejs/test-offline-funasr-nano.js:24:22)
-    at Object.<anonymous> (/home/runner/work/sherpa-onnx/sherpa-onnx/scripts/nodejs/test-offline-funasr-nano.js:27:20)
-    at Module._compile (node:internal/modules/cjs/loader:1521:14)
-    at Module._extensions..js (node:internal/modules/cjs/loader:1623:10)
-Node.js v20.19.6
-
- */
 const fs = require('fs');
 const {Readable} = require('stream');
 const wav = require('wav');
