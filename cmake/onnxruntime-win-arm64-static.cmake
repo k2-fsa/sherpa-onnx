b65b9361027aaa22847385f80db97cebc5cb48b3
Fangjun Kuang <csukuangfj@gmail.com>
2026-02-02 12:56:03 +0800

    Fix MSVC CRT for Windows arm64 (#3117)
    This pull request significantly enhances the build configuration for ONNX Runtime on Windows ARM64 by introducing greater flexibility in how the Microsoft Visual C++ Runtime (CRT) is linked and by supporting a full range of build types. This change allows developers to more precisely control their build environment, ensuring better compatibility and more robust dependency management for both static and shared ONNX Runtime libraries.

--- cmake/onnxruntime-win-arm64-static.cmake
+++ cmake/onnxruntime-win-arm64-static.cmake
@@ -15,22 +15,43 @@ if(BUILD_SHARED_LIBS)
   message(FATAL_ERROR "This file is for building static libraries. BUILD_SHARED_LIBS: ${BUILD_SHARED_LIBS}")
 endif()
 
-if(NOT CMAKE_BUILD_TYPE STREQUAL Release)
-  message(FATAL_ERROR "This file is for building a release version on Windows arm64")
+# Hashes for static CRT (/MT)
+set(ONNXRUNTIME_HASH_MT_Release "SHA256=03166e5c7a830586b8772ff166611f0806fbc0ca76bcd177113fe3275a8af59b")
+set(ONNXRUNTIME_HASH_MT_Debug "SHA256=61941c8d3058ebb6f9b83c9a99e4ae840708cc99702921d7c950a071f04f26ed")
+set(ONNXRUNTIME_HASH_MT_RelWithDebInfo "SHA256=304bd830680b773ed1fce35758f317f2b50b2278359ede292e33bd67b57290b7")
+set(ONNXRUNTIME_HASH_MT_MinSizeRel "SHA256=c24f46f689f9dbb8d8cd86d4c1d83f091da82d78548371d59446566d787f8cf4")
+
+# Hashes for dynamic CRT (/MD)
+set(ONNXRUNTIME_HASH_MD_Release "SHA256=26bae6d13335ecb229baa545d8c3b910998ace4f3617a4046640b9e6ef208dd7")
+set(ONNXRUNTIME_HASH_MD_Debug "SHA256=f3e6d4550ac00c9f8f7ef647974087627f41d063e9899b67a028cca6c34521ab")
+set(ONNXRUNTIME_HASH_MD_RelWithDebInfo "SHA256=ddef98c48243b0d7209edb9d416566405fd793551f63962fbb4f049b899136b0")
+set(ONNXRUNTIME_HASH_MD_MinSizeRel "SHA256=4b28704e04f25b0839004ca828306f387814ada953750c4103f7076768fcf8a1")
+
+if(NOT (CMAKE_BUILD_TYPE STREQUAL Release OR CMAKE_BUILD_TYPE STREQUAL Debug OR CMAKE_BUILD_TYPE STREQUAL RelWithDebInfo OR CMAKE_BUILD_TYPE STREQUAL MinSizeRel))
+  message(FATAL_ERROR "Supported CMAKE_BUILD_TYPE values are: Release, Debug, RelWithDebInfo, MinSizeRel. Given ${CMAKE_BUILD_TYPE}")
 endif()
 
-set(onnxruntime_URL  "https://github.com/csukuangfj/onnxruntime-libs/releases/download/v1.23.2/onnxruntime-win-arm64-static_lib-1.23.2.tar.bz2")
-set(onnxruntime_URL2 "https://hf-mirror.com/csukuangfj/onnxruntime-libs/resolve/main/1.23.2/onnxruntime-win-arm64-static_lib-1.23.2.tar.bz2")
-set(onnxruntime_HASH "SHA256=d3d1a6dd8e886e47f00e17dea521642b029c04936e52673109ef3063b31ad708")
+if(SHERPA_ONNX_USE_STATIC_CRT)
+  set(onnxruntime_crt "MT")
+else()
+  set(onnxruntime_crt "MD")
+endif()
+
+message(STATUS "Use MSVC CRT: ${onnxruntime_crt}")
+
+set(onnxruntime_HASH "${ONNXRUNTIME_HASH_${onnxruntime_crt}_${CMAKE_BUILD_TYPE}}")
+set(onnxruntime_filename "onnxruntime-win-arm64-static_lib-${onnxruntime_crt}-${CMAKE_BUILD_TYPE}-1.23.2.tar.bz2")
+set(onnxruntime_URL  "https://github.com/csukuangfj/onnxruntime-libs/releases/download/v1.23.2/${onnxruntime_filename}")
 
 # If you don't have access to the Internet,
 # please download onnxruntime to one of the following locations.
 # You can add more if you want.
 set(possible_file_locations
-  $ENV{HOME}/Downloads/onnxruntime-win-arm64-static_lib-1.23.2.tar.bz2
-  ${CMAKE_SOURCE_DIR}/onnxruntime-win-arm64-static_lib-1.23.2.tar.bz2
-  ${CMAKE_BINARY_DIR}/onnxruntime-win-arm64-static_lib-1.23.2.tar.bz2
-  /tmp/onnxruntime-win-arm64-static_lib-1.23.2.tar.bz2
+  $ENV{HOME}/Downloads/${onnxruntime_filename}
+  ${CMAKE_SOURCE_DIR}/${onnxruntime_filename}
+  ${CMAKE_BINARY_DIR}/${onnxruntime_filename}
+  $ENV{TMP}/${onnxruntime_filename}
+  $ENV{TEMP}/${onnxruntime_filename}
 )
 
 foreach(f IN LISTS possible_file_locations)
--- cmake/onnxruntime-win-arm64.cmake
+++ cmake/onnxruntime-win-arm64.cmake
@@ -15,18 +15,43 @@ if(NOT BUILD_SHARED_LIBS)
   message(FATAL_ERROR "This file is for building shared libraries. BUILD_SHARED_LIBS: ${BUILD_SHARED_LIBS}")
 endif()
 
-set(onnxruntime_URL  "https://github.com/microsoft/onnxruntime/releases/download/v1.23.2/onnxruntime-win-arm64-1.23.2.zip")
-set(onnxruntime_URL2 "https://hf-mirror.com/csukuangfj/onnxruntime-libs/resolve/main/1.23.2/onnxruntime-win-arm64-1.23.2.zip")
-set(onnxruntime_HASH "SHA256=1cfe88b6435df3b5fb0e9f6bd7d6f5df1e887b6174de7f6e2a47bab956f3f168")
+if(NOT (CMAKE_BUILD_TYPE STREQUAL Debug OR CMAKE_BUILD_TYPE STREQUAL RelWithDebInfo OR CMAKE_BUILD_TYPE STREQUAL MinSizeRel OR CMAKE_BUILD_TYPE STREQUAL Release))
+  message(FATAL_ERROR "Please set CMAKE_BUILD_TYPE to Release, Debug, RelWithDebInfo or MinSizeRel")
+endif()
+
+# Hashes for static CRT (/MT)
+set(ONNXRUNTIME_HASH_MT_Debug "SHA256=c9329ff4e8acdd0a07b40465d6521e15bb21eb0a4d9bc7843803e460dc4d02f0")
+set(ONNXRUNTIME_HASH_MT_RelWithDebInfo "SHA256=342470bef5681452fb9add5668debc5e79b5cd01a8c8866fc7c47f81dbd8eb70")
+set(ONNXRUNTIME_HASH_MT_MinSizeRel "SHA256=7bcbc8fd66fa1b0783dfdbd66eeb9ad4c023a1080ccc01e80412c2347aeddfa1")
+set(ONNXRUNTIME_HASH_MT_Release "SHA256=ee3c257f4c56f91a0a6aa3cce9a29dcd654f432fe5a399246c8bdb86c9bb5900")
+
+# Hashes for dynamic CRT (/MD)
+set(ONNXRUNTIME_HASH_MD_Debug "SHA256=722f044c84947e37fec7941f5dc38aa8f71cdf0b4bfa57cb590a4ed634b36ddc")
+set(ONNXRUNTIME_HASH_MD_RelWithDebInfo "SHA256=025b0dd682309482b3146b5c3a80e814ad9dec1e93ee8139954857b97d5798a9")
+set(ONNXRUNTIME_HASH_MD_MinSizeRel "SHA256=b8d5d508b21b4604d241ac11384fa6906daf556c6667011d9a8c6806d9549b74")
+set(ONNXRUNTIME_HASH_MD_Release "SHA256=08ed42a71fbce04e10a3192510a2c578a20c1d3a00652187f85002c54db84548")
+
+if(SHERPA_ONNX_USE_STATIC_CRT)
+  set(onnxruntime_crt "MT")
+else()
+  set(onnxruntime_crt "MD")
+endif()
+
+message(STATUS "Use MSVC CRT: ${onnxruntime_crt}")
+
+set(onnxruntime_filename "onnxruntime-win-arm64-${onnxruntime_crt}-${CMAKE_BUILD_TYPE}-1.23.2.tar.bz2")
+set(onnxruntime_URL  "https://github.com/csukuangfj/onnxruntime-libs/releases/download/v1.23.2/${onnxruntime_filename}")
+set(onnxruntime_HASH "${ONNXRUNTIME_HASH_${onnxruntime_crt}_${CMAKE_BUILD_TYPE}}")
 
 # If you don't have access to the Internet,
 # please download onnxruntime to one of the following locations.
 # You can add more if you want.
 set(possible_file_locations
-  $ENV{HOME}/Downloads/onnxruntime-win-arm64-1.23.2.zip
-  ${CMAKE_SOURCE_DIR}/onnxruntime-win-arm64-1.23.2.zip
-  ${CMAKE_BINARY_DIR}/onnxruntime-win-arm64-1.23.2.zip
-  /tmp/onnxruntime-win-arm64-1.23.2.zip
+  $ENV{HOME}/Downloads/${onnxruntime_filename}
+  ${CMAKE_SOURCE_DIR}/${onnxruntime_filename}
+  ${CMAKE_BINARY_DIR}/${onnxruntime_filename}
+  $ENV{TMP}/${onnxruntime_filename}
+  $ENV{TEMP}/${onnxruntime_filename}
 )
 
 foreach(f IN LISTS possible_file_locations)
