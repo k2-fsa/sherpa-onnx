dbe7c7e7d3c4d5108fe8228a2a80b372eb12f286
Fangjun Kuang <csukuangfj@gmail.com>
2026-02-03 21:08:49 +0800

    Upload WenetSpeech-Wu u2pp ASR models. (#3125)

--- sherpa-onnx/csrc/offline-recognizer-ctc-impl.h
+++ sherpa-onnx/csrc/offline-recognizer-ctc-impl.h
@@ -151,6 +151,7 @@ class OfflineRecognizerCtcImpl : public OfflineRecognizerImpl {
       // WeNet CTC models assume input samples are in the range
       // [-32768, 32767], so we set normalize_samples to false
       config_.feat_config.normalize_samples = false;
+      config_.feat_config.dither = 1;
     }
 
     if (!config_.model_config.medasr.model.empty()) {
