e313e154b88b0e0b97c46ed4a81bf107a8aa16e7
Fangjun Kuang <csukuangfj@gmail.com>
2026-02-06 14:16:22 +0800

    Export models for CANN 8.1 (#3152)

--- .github/scripts/export-ascend/generate_zipformer_ctc_20250703.py
+++ .github/scripts/export-ascend/generate_zipformer_ctc_20250703.py
@@ -16,12 +16,14 @@ def get_image(cann: str, soc_version: str):
     cann2image_910 = {
         "7.0": "quay.io/ascend/cann:7.0.1.beta1-910b-ubuntu22.04-py3.8",
         "8.0": "gpustack/ascendai-cann:8.0.RC3-910b-ubuntu20.04-py3.9",
+        "8.1": "gpustack/devel-ascendai-cann:8.1.rc1.beta1-910b-ubuntu20.04-v2",
         "8.2": "gpustack/devel-ascendai-cann:8.2.rc1-910b-ubuntu20.04-v2",
     }
 
     cann2image_310 = {
         "7.0": "quay.io/ascend/cann:7.0.1-310p-ubuntu22.04-py3.9",
         "8.0": "gpustack/devel-ascendai-cann:8.0.rc3.beta1-310p-ubuntu20.04-v2",
+        "8.1": "gpustack/devel-ascendai-cann:8.1.rc1.beta1-310p-ubuntu20.04-v2",
         "8.2": "gpustack/devel-ascendai-cann:8.2.rc1-310p-ubuntu20.04-v2",
     }
     if "910" in soc_version:
@@ -38,13 +40,13 @@ def get_soc_version():
 
 
 def get_cann_version():
-    cann_version = ["7.0", "8.0", "8.2"]
+    cann_version = ["7.0", "8.0", "8.1", "8.2"]
     return cann_version
 
 
 @dataclass
 class Config:
-    # 7.0, 8.0, 8.2
+    # 7.0, 8.0, 8.1, 8.2
     cann: str
 
     # 910B, 910B2, 910B3, 310P3
