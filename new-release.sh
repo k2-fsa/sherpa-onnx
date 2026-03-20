e0ab4a8beb10477d25ef48e518a537995c38c698
Fangjun Kuang <csukuangfj@gmail.com>
2026-03-20 19:16:04 +0800

    Release v1.12.31 (#3379)

--- new-release.sh
+++ new-release.sh
@@ -2,11 +2,11 @@
 
 set -ex
 
-old_version_code=20260312
-new_version_code=20260319
+old_version_code=20260319
+new_version_code=20260320
 
-old_version="1\.12\.29"
-new_version="1\.12\.30"
+old_version="1\.12\.30"
+new_version="1\.12\.31"
 
 replace_str="s/$old_version/$new_version/g"
 
