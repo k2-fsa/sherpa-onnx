ca22668659c48699ab765fc61e16fc4374a2fe7f
Fangjun Kuang <csukuangfj@gmail.com>
2026-03-26 13:42:24 +0800

    Refactor Go API examples to use sherpa_onnx.ReadWave() (#3415)
    This pull request refactors the Go API examples by replacing the custom WAV file reading implementations with the dedicated sherpa_onnx.ReadWave() function. This change streamlines the audio input process, removes redundant code, and leverages the library's built-in capabilities for handling audio waveforms, resulting in cleaner and more consistent example code.

--- go-api-examples/streaming-decode-files/main.go
+++ go-api-examples/streaming-decode-files/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
+	"log"
+	"strings"
+
 	sherpa "github.com/k2-fsa/sherpa-onnx-go/sherpa_onnx"
 	flag "github.com/spf13/pflag"
-	"github.com/youpy/go-wav"
-	"log"
-	"os"
-	"strings"
 )
 
 func main() {
@@ -46,9 +43,13 @@ func main() {
 		log.Fatalf("Please provide one wave file")
 	}
 
-	log.Println("Reading", flag.Arg(0))
+	waveFilename := flag.Arg(0)
+	log.Println("Reading", waveFilename)
 
-	samples, sampleRate := readWave(flag.Arg(0))
+	wave := sherpa.ReadWave(waveFilename)
+	if wave == nil {
+		log.Fatalf("Failed to read %v", waveFilename)
+	}
 
 	log.Println("Initializing recognizer (may take several seconds)")
 	recognizer := sherpa.NewOnlineRecognizer(&config)
@@ -59,13 +60,13 @@ func main() {
 	stream := sherpa.NewOnlineStream(recognizer)
 	defer sherpa.DeleteOnlineStream(stream)
 
-	leftPadding := make([]float32, int(float32(sampleRate)*0.3))
-	stream.AcceptWaveform(sampleRate, leftPadding)
+	leftPadding := make([]float32, int(float32(wave.SampleRate)*0.3))
+	stream.AcceptWaveform(wave.SampleRate, leftPadding)
 
-	stream.AcceptWaveform(sampleRate, samples)
+	stream.AcceptWaveform(wave.SampleRate, wave.Samples)
 
-	tailPadding := make([]float32, int(float32(sampleRate)*0.6))
-	stream.AcceptWaveform(sampleRate, tailPadding)
+	tailPadding := make([]float32, int(float32(wave.SampleRate)*0.6))
+	stream.AcceptWaveform(wave.SampleRate, tailPadding)
 
 	for recognizer.IsReady(stream) {
 		recognizer.Decode(stream)
@@ -73,60 +74,5 @@ func main() {
 	log.Println("Decoding done!")
 	result := recognizer.GetResult(stream)
 	log.Println(strings.ToLower(result.Text))
-	log.Printf("Wave duration: %v seconds", float32(len(samples))/float32(sampleRate))
-}
-
-func readWave(filename string) (samples []float32, sampleRate int) {
-	file, _ := os.Open(filename)
-	defer file.Close()
-
-	reader := wav.NewReader(file)
-	format, err := reader.Format()
-	if err != nil {
-		log.Fatalf("Failed to read wave format")
-	}
-
-	if format.AudioFormat != 1 {
-		log.Fatalf("Support only PCM format. Given: %v\n", format.AudioFormat)
-	}
-
-	if format.NumChannels != 1 {
-		log.Fatalf("Support only 1 channel wave file. Given: %v\n", format.NumChannels)
-	}
-
-	if format.BitsPerSample != 16 {
-		log.Fatalf("Support only 16-bit per sample. Given: %v\n", format.BitsPerSample)
-	}
-
-	reader.Duration() // so that it initializes reader.Size
-
-	buf := make([]byte, reader.Size)
-	n, err := reader.Read(buf)
-	if n != int(reader.Size) {
-		log.Fatalf("Failed to read %v bytes. Returned %v bytes\n", reader.Size, n)
-	}
-
-	samples = samplesInt16ToFloat(buf)
-	sampleRate = int(format.SampleRate)
-
-	return
-}
-
-func samplesInt16ToFloat(inSamples []byte) []float32 {
-	numSamples := len(inSamples) / 2
-	outSamples := make([]float32, numSamples)
-
-	for i := 0; i != numSamples; i++ {
-		s := inSamples[i*2 : (i+1)*2]
-
-		var s16 int16
-		buf := bytes.NewReader(s)
-		err := binary.Read(buf, binary.LittleEndian, &s16)
-		if err != nil {
-			log.Fatal("Failed to parse 16-bit sample")
-		}
-		outSamples[i] = float32(s16) / 32768
-	}
-
-	return outSamples
+	log.Printf("Wave duration: %v seconds", float32(len(wave.Samples))/float32(wave.SampleRate))
 }
