7d618653d592de88c52961d4b11fee62c1624b5c
Fangjun Kuang <csukuangfj@gmail.com>
2026-03-27 14:15:11 +0800

    Add Go API for source separation (#3432)

---
+++ scripts/go/_internal/source-separation/run-uvr.sh
@@ -0,0 +1,1 @@
+../../../../go-api-examples/source-separation/run-uvr.sh
--- scripts/go/sherpa_onnx.go
+++ scripts/go/sherpa_onnx.go
@@ -164,10 +164,10 @@ type OnlineRecognizerConfig struct {
 
 // It contains the recognition result for a online stream.
 type OnlineRecognizerResult struct {
-	Text      string
-	Tokens    []string
+	Text       string
+	Tokens     []string
 	Timestamps []float32
-	Json      string
+	Json       string
 }
 
 // The online recognizer class. It wraps a pointer from C.
@@ -535,15 +535,15 @@ type OfflineMoonshineModelConfig struct {
 }
 
 type OfflineQwen3ASRModelConfig struct {
-	ConvFrontend  string
-	Encoder       string
-	Decoder       string
-	Tokenizer     string
-	MaxTotalLen   int
-	MaxNewTokens  int
-	Temperature   float32
-	TopP          float32
-	Seed          int
+	ConvFrontend string
+	Encoder      string
+	Decoder      string
+	Tokenizer    string
+	MaxTotalLen  int
+	MaxNewTokens int
+	Temperature  float32
+	TopP         float32
+	Seed         int
 }
 
 type OfflineTdnnModelConfig struct {
@@ -2719,4 +2719,209 @@ func GetGitSha1() string {
 
 func GetGitDate() string {
 	return C.GoString(C.SherpaOnnxGetGitDate())
+}
+
+type OfflineSourceSeparationSpleeterModelConfig struct {
+	Vocals        string
+	Accompaniment string
+}
+
+// UvrConfig wraps SherpaOnnxOfflineSourceSeparationUvrModelConfig
+type OfflineSourceSeparationUvrModelConfig struct {
+	Model string
+}
+
+type OfflineSourceSeparationModelConfig struct {
+	Spleeter   OfflineSourceSeparationSpleeterModelConfig
+	Uvr        OfflineSourceSeparationUvrModelConfig
+	NumThreads int
+	Debug      bool
+	Provider   string // e.g., "cpu", "cuda", "coreml"
+}
+
+// Config is the top-level configuration class
+type OfflineSourceSeparationConfig struct {
+	Model OfflineSourceSeparationModelConfig
+}
+
+type AudioBuffer struct {
+	Samples           []float32
+	ChannelCount      int
+	SampleRate        int
+	SamplesPerChannel int
+	cWave             *C.SherpaOnnxMultiChannelWave // Pointer if C-managed
+}
+
+// NewAudioBuffer creates a buffer from Go-managed memory
+func NewAudioBuffer(samples []float32, channelCount int, sampleRate int) *AudioBuffer {
+	return &AudioBuffer{
+		Samples:           samples,
+		ChannelCount:      channelCount,
+		SampleRate:        sampleRate,
+		SamplesPerChannel: len(samples) / channelCount,
+	}
+}
+
+// ReadWave reads from disk into C-managed memory (Zero-Copy)
+// Note that you have to use AudioBuffer.Release() to avoid memory leak
+func ReadWaveMultiChannel(filename string) *AudioBuffer {
+	cStr := C.CString(filename)
+	defer C.free(unsafe.Pointer(cStr))
+
+	ptr := C.SherpaOnnxReadWaveMultiChannel(cStr)
+	if ptr == nil {
+		return nil
+	}
+
+	buf := &AudioBuffer{
+		ChannelCount:      int(ptr.num_channels),
+		SamplesPerChannel: int(ptr.num_samples),
+		SampleRate:        int(ptr.sample_rate),
+		cWave:             ptr,
+	}
+
+	total := buf.ChannelCount * buf.SamplesPerChannel
+	// View C memory as a Go slice
+	buf.Samples = (*[1 << 30]float32)(unsafe.Pointer(*ptr.samples))[:total:total]
+	return buf
+}
+
+// Release manually frees C-allocated memory
+func (b *AudioBuffer) Release() {
+	if b.cWave != nil {
+		C.SherpaOnnxFreeMultiChannelWave(b.cWave)
+		b.cWave = nil
+		b.Samples = nil
+	}
+}
+
+func (b *AudioBuffer) Save(filename string) bool {
+	if len(b.Samples) == 0 {
+		return false
+	}
+	cStr := C.CString(filename)
+	defer C.free(unsafe.Pointer(cStr))
+
+	// Allocate the pointer array in C memory to avoid "Go pointer to Go pointer" panic.
+	// We need an array of (float*) with size (ChannelCount).
+	ptrSize := unsafe.Sizeof((*C.float)(nil))
+	cPtrs := (*[1 << 20]*C.float)(C.malloc(C.size_t(uintptr(b.ChannelCount) * ptrSize)))
+	defer C.free(unsafe.Pointer(cPtrs))
+
+	for i := 0; i < b.ChannelCount; i++ {
+		offset := i * b.SamplesPerChannel
+		// It is safe to pass a pointer to Go memory as a C argument,
+		// but NOT as a member of a Go-allocated struct/slice passed to C.
+		cPtrs[i] = (*C.float)(unsafe.Pointer(&b.Samples[offset]))
+	}
+
+	res := C.SherpaOnnxWriteWaveMultiChannel(
+		(**C.float)(unsafe.Pointer(cPtrs)),
+		C.int32_t(b.SamplesPerChannel),
+		C.int32_t(b.SampleRate),
+		C.int32_t(b.ChannelCount),
+		cStr,
+	)
+	return res == 1
+}
+
+type SourceSeparator struct {
+	handle *C.SherpaOnnxOfflineSourceSeparation
+}
+
+// Please use SourceSeparator.Delete() to avoid memory leak
+func NewSourceSeparator(cfg OfflineSourceSeparationConfig) *SourceSeparator {
+	var cCfg C.SherpaOnnxOfflineSourceSeparationConfig
+
+	// Map Model Config
+	cCfg.model.num_threads = C.int32_t(cfg.Model.NumThreads)
+	if cfg.Model.Debug {
+		cCfg.model.debug = 1
+	}
+
+	// Helper to track and free C strings
+	var allocated []*C.char
+	toC := func(s string) *C.char {
+		if s == "" {
+			return nil
+		}
+		ptr := C.CString(s)
+		allocated = append(allocated, ptr)
+		return ptr
+	}
+
+	// String conversions
+	cCfg.model.provider = toC(cfg.Model.Provider)
+	cCfg.model.spleeter.vocals = toC(cfg.Model.Spleeter.Vocals)
+	cCfg.model.spleeter.accompaniment = toC(cfg.Model.Spleeter.Accompaniment)
+	cCfg.model.uvr.model = toC(cfg.Model.Uvr.Model)
+
+	// Free strings after creation call
+	defer func() {
+		for _, ptr := range allocated {
+			C.free(unsafe.Pointer(ptr))
+		}
+	}()
+
+	h := C.SherpaOnnxCreateOfflineSourceSeparation(&cCfg)
+	if h == nil {
+		return nil
+	}
+
+	return &SourceSeparator{handle: h}
+}
+
+func (ss *SourceSeparator) Delete() {
+	if ss.handle != nil {
+		C.SherpaOnnxDestroyOfflineSourceSeparation(ss.handle)
+		ss.handle = nil
+	}
+}
+
+func (ss *SourceSeparator) Process(buf *AudioBuffer) []*AudioBuffer {
+	if ss.handle == nil || len(buf.Samples) == 0 {
+		return nil
+	}
+
+	// FIX: Allocate the pointer array in C memory
+	ptrSize := unsafe.Sizeof((*C.float)(nil))
+	cPtrs := (*[1 << 20]*C.float)(C.malloc(C.size_t(uintptr(buf.ChannelCount) * ptrSize)))
+	defer C.free(unsafe.Pointer(cPtrs))
+
+	for i := 0; i < buf.ChannelCount; i++ {
+		offset := i * buf.SamplesPerChannel
+		cPtrs[i] = (*C.float)(unsafe.Pointer(&buf.Samples[offset]))
+	}
+
+	cOut := C.SherpaOnnxOfflineSourceSeparationProcess(
+		ss.handle,
+		(**C.float)(unsafe.Pointer(cPtrs)),
+		C.int32_t(buf.ChannelCount),
+		C.int32_t(buf.SamplesPerChannel),
+		C.int32_t(buf.SampleRate),
+	)
+	if cOut == nil {
+		return nil
+	}
+	defer C.SherpaOnnxDestroySourceSeparationOutput(cOut)
+
+	numStems := int(cOut.num_stems)
+	sampleRate := int(cOut.sample_rate)
+	stems := make([]*AudioBuffer, numStems)
+
+	for i := 0; i < numStems; i++ {
+		cStem := (*[1 << 20]C.SherpaOnnxSourceSeparationStem)(unsafe.Pointer(cOut.stems))[i]
+		chCount := int(cStem.num_channels)
+		sCount := int(cStem.n)
+
+		flat := make([]float32, chCount*sCount)
+		for c := 0; c < chCount; c++ {
+			cChannelPtr := (*[1 << 20]*C.float)(unsafe.Pointer(cStem.samples))[c]
+			source := (*[1 << 30]float32)(unsafe.Pointer(cChannelPtr))[:sCount:sCount]
+			copy(flat[c*sCount:], source)
+		}
+		stems[i] = NewAudioBuffer(flat, chCount, sampleRate)
+	}
+
+	return stems
 }
