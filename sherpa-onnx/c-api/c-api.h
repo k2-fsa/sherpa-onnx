c060d598ef88ddb86910ee3edeacca45bc8b8cfd
Fangjun Kuang <csukuangfj@gmail.com>
2026-03-27 12:08:38 +0800

    Begin to add Go API for source separation

--- sherpa-onnx/c-api/c-api.h
+++ sherpa-onnx/c-api/c-api.h
@@ -2830,7 +2830,8 @@ SHERPA_ONNX_API void SherpaOnnxFreeWave(const SherpaOnnxWave *wave);
  */
 typedef struct SherpaOnnxMultiChannelWave {
   /** samples[c] points to channel c samples normalized to [-1, 1].
-   * Note that samples[c] and samples[c+1] are contiguous in memory.
+   * Note: The sample data for all channels are stored in a single contiguous
+   * memory block, one channel after another.
    * */
   const float *const *samples;
   /** Number of channels. */
