40e51ab5d0d4b6374125daabc0a8afcaefb99cec
Fangjun Kuang <csukuangfj@gmail.com>
2026-04-03 11:43:12 +0800

    Release v1.12.35

--- android/SherpaOnnxSimulateStreamingAsr/app/build.gradle.kts
+++ android/SherpaOnnxSimulateStreamingAsr/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.k2fsa.sherpa.onnx.simulate.streaming.asr"
         minSdk = 21
         targetSdk = 34
-        versionCode = 20260326
-        versionName = "1.12.34"
+        versionCode = 20260403
+        versionName = "1.12.35"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
