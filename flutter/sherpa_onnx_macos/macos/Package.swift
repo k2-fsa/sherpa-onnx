f330fdb1c1b182839691ddd26f8f060a2c6fc550
huangj <xyhuangjia@gmail.com>
2026-05-31 17:25:55 +0800

    feat: add macOS Swift Package Manager support

---
+++ flutter/sherpa_onnx_macos/macos/Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "sherpa_onnx_macos",
+    platforms: [.macOS(.v10_11)],
+    products: [
+        .library(name: "sherpa_onnx_macos", targets: ["sherpa_onnx_macos"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "sherpa_onnx_macos",
+            path: "sherpa_onnx.xcframework"
+        ),
+    ]
+)
