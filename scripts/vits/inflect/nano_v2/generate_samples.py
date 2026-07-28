f38d826ae64073cf6381d9d7dad4ec085af63f5e
Fangjun Kuang <csukuangfj@gmail.com>
2026-07-28 16:03:00 +0800

    Generate TTS samples for Inflect TTS

---
+++ scripts/vits/inflect/nano_v2/generate_samples.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# Copyright    2026  Xiaomi Corp.        (authors: Fangjun Kuang)
+"""
+Generate samples for
+https://k2-fsa.github.io/sherpa/onnx/tts/all/
+"""
+
+import os
+from pathlib import Path
+
+import sherpa_onnx
+import soundfile as sf
+
+config = sherpa_onnx.OfflineTtsConfig(
+    model=sherpa_onnx.OfflineTtsModelConfig(
+        vits=sherpa_onnx.OfflineTtsVitsModelConfig(
+            model="vits-inflect-en-nano-v2/model.onnx",
+            data_dir="vits-inflect-en-nano-v2/espeak-ng-data",
+            tokens="vits-inflect-en-nano-v2/tokens.txt",
+        ),
+        num_threads=2,
+    ),
+    max_num_sentences=1,
+)
+
+if not config.validate():
+    raise ValueError("Please check your config")
+
+if not config.validate():
+    raise ValueError("Please check your config")
+
+tts = sherpa_onnx.OfflineTts(config)
+text = "Friends fell out often because life was changing so fast. The easiest thing in the world was to lose touch with someone."
+
+audio = tts.generate(text, sid=0, speed=1.0)
+
+sf.write(
+    "./hf/inflect/vits-inflect-en-nano-v2/mp3/0.mp3",
+    audio.samples,
+    samplerate=audio.sample_rate,
+)
