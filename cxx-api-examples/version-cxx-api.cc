d1fedc1daac9304cd8f85350a38aed2e5e120f02
xiaotong-spacemit <xiaotong.niu@spacemit.com>
2026-07-28 20:58:04 +0800

    build(cmake): update spacemit-ort version (#3814)

--- cxx-api-examples/version-cxx-api.cc
+++ cxx-api-examples/version-cxx-api.cc
@@ -7,11 +7,12 @@
 #include "sherpa-onnx/c-api/cxx-api.h"
 
 auto main() -> int {
-  std::cout << "sherpa-onnx version : " << sherpa_onnx::GetVersionStr()
+  using namespace sherpa_onnx::cxx;  // NOLINT
+  std::cout << "sherpa-onnx version : " << GetVersionStr()
             << "\n";
-  std::cout << "sherpa-onnx Git SHA1: " << sherpa_onnx::GetGitSha1() << "\n";
-  std::cout << "sherpa-onnx Git date: " << sherpa_onnx::GetGitDate() << "\n";
-  std::cout << "onnxruntime version : " << sherpa_onnx::GetOnnxruntimeVersionStr()
+  std::cout << "sherpa-onnx Git SHA1: " << GetGitSha1() << "\n";
+  std::cout << "sherpa-onnx Git date: " << GetGitDate() << "\n";
+  std::cout << "onnxruntime version : " << GetOnnxruntimeVersionStr()
             << "\n";
 
   return 0;
