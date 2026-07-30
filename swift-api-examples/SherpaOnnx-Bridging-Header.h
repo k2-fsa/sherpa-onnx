5e4232db78d0150801ae3244c9e2ddc41e5e02d8
Fangjun Kuang <csukuangfj@gmail.com>
2026-07-30 11:46:54 +0800

    Add missing files for swift package manager (#3828)

---
+++ swift-api-examples/SherpaOnnx-Bridging-Header.h
@@ -0,0 +1,9 @@
+// swfit-api-examples/SherpaOnnx-Bridging-Header.h
+//
+// Copyright (c)  2023  Xiaomi Corporation
+#ifndef SWIFT_API_EXAMPLES_SHERPAONNX_BRIDGING_HEADER_H_
+#define SWIFT_API_EXAMPLES_SHERPAONNX_BRIDGING_HEADER_H_
+
+#import "sherpa-onnx/c-api/c-api.h"
+
+#endif  // SWIFT_API_EXAMPLES_SHERPAONNX_BRIDGING_HEADER_H_
