bec5b2e400c908f0a7dfb3265bd1bc0c2e957552
zhengfeng <zhengfeng@cri-tsing.com>
2026-09-22 17:15:02 +0800

    default to 16k for backward compatibility

--- sherpa-onnx/csrc/online-websocket-server-impl.h
+++ sherpa-onnx/csrc/online-websocket-server-impl.h
@@ -67,7 +67,7 @@ struct OnlineWebsocketDecoderConfig {
   // The sample rate of input audio from the client.
   // If it differs from the model's expected sample rate (e.g. 16000),
   // the server will resample internally.
-  int32_t input_sample_rate = 8000;
+  int32_t input_sample_rate = 16000;
 
   void Register(ParseOptions *po);
   void Validate() const;
